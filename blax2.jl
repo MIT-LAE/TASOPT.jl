1f98301167ec7ba8f09c8900d1d6977a664dffcb
Prashanth <prash@mit.edu>
2020-12-01 19:53:18 -0500

    Cosemetic changes to test unicode characters in Julia

--- blax2.jl
+++ blax2.jl
@@ -1,7 +1,7 @@
 
-function blax2(ndim, n,ite, xi,bi,rni, uinv, Reyn, Mach, fexcr )
+function blax2(ndim, n,ite, xᵢ,bi,rni, uinv, Reyn, Mach, fexcr )
 #-----------------------------------------------------------------
-#     Axisymmetric boundary layer + wake calculation routine.
+#     Axᵢsymmetric boundary layer + wake calculation routine.
 #     Uses specified inviscid velocity, corrects for viscous
 #     displacement to allow calculation of separated flow.
 #
@@ -10,73 +10,73 @@ function blax2(ndim, n,ite, xi,bi,rni, uinv, Reyn, Mach, fexcr )
 #    ndim    physical array dimension
 #    n       number of BL+wake points
 #    ite     index of trailing edge point, start of wake
-#    xi(.)   arc length array (BL coordinate)
+#    xᵢ(.)   arc length array (BL coordinate)
 #    bi(.)   lateral width of BL (body perimeter, zero for wake)
 #             = 1 for 2D
 #    rni(.)  dr/dn
 #             = 0 for 2D
 #    uinv(.) inviscid velocity
-#    Reyn    Reynolds number,  rho_ref u_ref l_ref / mu_ref
+#    Reyn    Reynolds number,  ρ_ref u_ref l_ref / mu_ref
 #    Mach    Mach number    ,  u_ref / a_ref
 #    fexcr   excrescence multiplier, applied to wall Cf 
 #             = 1 for smooth wall
 #
 #  Assumed units for all quantities:
-#    l_ref   same unit as used for input xi,bi
+#    l_ref   same unit as used for input xᵢ,bi
 #    u_ref   freestream velocity
 #    a_ref   freestream speed of sound
-#    rho_ref freestream density
+#    ρ_ref freestream density
 #    mu_ref  freestream viscosity
 #    
 #  Outputs
 #  -------
-#    uei[i]  edge velocity, ( = uinv[i] + displacement correction )
-#    dsi[i]  displacement thickness
-#    thi[i]  momentum thickness
-#    tsi[i]  kinetic energy thickness
-#    dci[i]  density flux thickness
-#    cfi[i]  skin friction coefficient, normalized with local rho,u
-#    cdi[i]  dissipation coefficient  , normalized with local rho,u
-#    cti[i]  max shear-stress coefficient, normalized with local rho,u
+#    uₑᵢ[i]  edge velocity, ( = uinv[i] + displacement correction )
+#    δᵢ[i]  displacement θᵢckness
+#    θᵢ[i]  momentum θᵢckness
+#    θ∗ᵢ[i]  kinetic energy θᵢckness
+#    δᵢ⁺⁺[i]  density flux θᵢckness
+#    cfi[i]  skin friction coefficient, normalized with local ρ,u
+#    cdi[i]  dissipation coefficient  , normalized with local ρ,u
+#    cti[i]  max shear-stress coefficient, normalized with local ρ,u
 #    hki[i]  kinematic shape parameter
-#    phi[i]  integrated dissipation
+#    ϕ[i]  integrated dissipation
 #
 #
 #  Other outputs of interest can be computed as follows.
-#  These are in units of l_ref, rho_ref, u_ref
+#  These are in units of l_ref, ρ_ref, u_ref
 #
-#  Effective perimeter  : beff  =  bi  +  2 pi dsi rni
-#  Edge density         : rhi = (1 + 0.5*(gam-1)*Mach^2*(1.0-uei^2))^(1/(gam-1))
-#  Total mass defect    : mdef =  rhi uei   dsi beff
-#  Total mom. defect    : Pdef =  rhi uei^2 thi beff
-#  Total KE defect      : Edef =  rhi uei^3 tsi beff / 2
-#  Wall shear force/span: tw b =  rhi uei^2 cfi beff / 2
-#  Dissipation integral : Diss =  rhi uei^3 cdi beff
+#  Effective perimeter  : beff  =  bi  +  2 π δᵢ rni
+#  Edge density         : ρᵢ = (1 + 0.5*(ɣ-1)*Mach^2*(1.0-uₑᵢ^2))^(1/(ɣ-1))
+#  Total mass defect    : mdef =  ρᵢ uₑᵢ   δᵢ beff
+#  Total mom. defect    : Pdef =  ρᵢ uₑᵢ^2 θᵢ beff
+#  Total KE defect      : Edef =  ρᵢ uₑᵢ^3 θ∗ᵢ beff / 2
+#  Wall shear force/span: tw b =  ρᵢ uₑᵢ^2 cfi beff / 2
+#  Dissipation integral : Diss =  ρᵢ uₑᵢ^3 cdi beff
 #
-#  Body profile drag Dp is the far-downstream momentum defect Pinf,
+#  Body profile drag Dp is the far-downstream momentum defect P∞,
 #  best obtained by applying Squire-Young to the last wake point i = n :
 #
-#   Pend = rhi*uei^2 * thi * beff
-#   Hend = dsi/thi
-#   Hinf = 1.0 + (gam-1)*Mach^2
-#   Havg = 0.5*(Hend+Hinf)
-#   Pinf = Pend * uei^Havg  =  Dp
+#   Pend = ρᵢ*uₑᵢ^2 * θᵢ * beff
+#   Hend = δᵢ/θᵢ
+#   H∞ = 1.0 + (ɣ-1)*Mach^2
+#   Havg = 0.5*(Hend+H∞)
+#   P∞ = Pend * uₑᵢ^Havg  =  Dp
 #
 #-----------------------------------------------------------------
 
-#      real xi(ndim), bi(ndim), rni(ndim), uinv(ndim),
+#      real xᵢ(ndim), bi(ndim), rni(ndim), uinv(ndim),
 
-      uei  = zeros(ndim)
-      rhi  = zeros(ndim)
-      dsi  = zeros(ndim)
-      thi  = zeros(ndim)
-      tsi  = zeros(ndim)
-      dci  = zeros(ndim)
+      uₑᵢ  = zeros(ndim)
+      ρᵢ  = zeros(ndim)
+      δᵢ  = zeros(ndim)
+      θᵢ  = zeros(ndim)
+      θ∗ᵢ  = zeros(ndim)
+      δᵢ⁺⁺  = zeros(ndim)
       cfi  = zeros(ndim)
       cdi  = zeros(ndim)
       cti  = zeros(ndim)
       hki  = zeros(ndim)
-      phi  = zeros(ndim)
+      ϕ  = zeros(ndim)
 
       aa = zeros(3,3)
       bb = zeros(3,3)
@@ -88,7 +88,7 @@ function blax2(ndim, n,ite, xi,bi,rni, uinv, Reyn, Mach, fexcr )
       hsm, hsm_thm, hsm_dsm, hsm_uem = 1e20*ones(4)
       cfm, cfm_thm, cfm_dsm, cfm_uem = 1e20*ones(4)
       dim, dim_thm, dim_dsm, dim_uem = 1e20*ones(4)
-      h , h_th, h_ds,
+      h , ∂h∂θ, ∂h∂δ,
       hk, hk_th, hk_ds, hk_ue,
       hc, hc_th, hc_ds, hc_ue,
       hs, hs_th, hs_ds, hs_ue,
@@ -100,10 +100,10 @@ function blax2(ndim, n,ite, xi,bi,rni, uinv, Reyn, Mach, fexcr )
       mdi      = zeros(idim)
       uvis     = zeros(idim)
       uvis_mdi = zeros(idim,idim)
-      dthi     = zeros(idim)
+      dθᵢ     = zeros(idim)
       dmdi     = zeros(idim)
-      duei     = zeros(idim)
-      ddsi     = zeros(idim)
+      duₑᵢ     = zeros(idim)
+      dδᵢ     = zeros(idim)
 
       kdim=3*n #idim
       asys = zeros(kdim,kdim)
@@ -111,9 +111,9 @@ function blax2(ndim, n,ite, xi,bi,rni, uinv, Reyn, Mach, fexcr )
 
       simi, lami, wake, direct = true, true, true, true
 
-#---- pi  and  1/(4 pi)
-      qopi  = 0.07957747154594766788444188168625718
-      gam   = 1.4
+#---- π  and  1/(4 π)
+      π¼  = 0.07957747154594766788444188168625718
+      ɣ   = 1.4
 
       hksep = 2.9
 
@@ -124,22 +124,22 @@ function blax2(ndim, n,ite, xi,bi,rni, uinv, Reyn, Mach, fexcr )
        quit()
       end
 
-      gmi = gam - 1.0
+      gmi = ɣ - 1.0
 
-#---- initialize ue to inviscid uinv, and initialize rhoe
+#---- initialize ue to inviscid uinv, and initialize ρe
       for i = 1: n
-        uei[i] = uinv[i]
+        uₑᵢ[i] = uinv[i]
 
-        trat = 1.0 + 0.5*gmi*Mach^2 * (1.0-uei[i]^2)
-        rhi[i] = trat^(1.0/gmi)
+        trat = 1.0 + 0.5*gmi*Mach^2 * (1.0-uₑᵢ[i]^2)
+        ρᵢ[i] = trat^(1.0/gmi)
       end
 
-#---- first point is not calculated if xi=0 there
-      if(xi[1] == 0.0) 
-       thi[1] = 0.
-       dsi[1] = 0.
+#---- first point is not calculated if xᵢ=0 there
+      if(xᵢ[1] == 0.0) 
+       θᵢ[1] = 0.
+       δᵢ[1] = 0.
        mdi[1] = 0.
-       phi[1] = 0.
+       ϕ[1] = 0.
       end
 
 # =============================================================================
@@ -149,18 +149,18 @@ function blax2(ndim, n,ite, xi,bi,rni, uinv, Reyn, Mach, fexcr )
 direct = true
 for i = 2: n #BL march loop
 
-        simi = xi[i-1] == 0.0
+        simi = xᵢ[i-1] == 0.0
 
 #c      lami = simi     # laminar   similarity station
         lami = false  # turbulent similarity station
 
         wake = i  > ite
 
-        x = xi[i]
+        x = xᵢ[i]
         b = bi[i]
         rn = rni[i]
 
-        xm = xi[i-1]
+        xm = xᵢ[i-1]
         bm = bi[i-1]
         rnm = rni[i-1]
 
@@ -170,24 +170,24 @@ for i = 2: n #BL march loop
          thm = 0.
          dsm = 0.
 
-         ue = uei[i]
+         ue = uₑᵢ[i]
          rex = ue*x * Reyn
          th = 0.4*x / sqrt(rex)
          ds = th * 2.0
 
         else
-         uem = uei[i-1]
-         thm = thi[i-1]
-         dsm = dsi[i-1]
+         uem = uₑᵢ[i-1]
+         thm = θᵢ[i-1]
+         dsm = δᵢ[i-1]
 
-         th = thi[i-1]
-         ds = dsi[i-1]
+         th = θᵢ[i-1]
+         ds = δᵢ[i-1]
          if(direct) 
 #-------- previous station was direct... use specified ue as initial guess
-          ue = uei[i]
+          ue = uₑᵢ[i]
          else
 #-------- previous station was inverse... use previous-station ue as initial guess
-          ue = uei[i-1]
+          ue = uₑᵢ[i-1]
          end
 
         end
@@ -198,7 +198,7 @@ for i = 2: n #BL march loop
 
         for iter = 1: 20 #Newton iteration
           
-          (h , h_th, h_ds,
+          (h , ∂h∂θ, ∂h∂δ,
           hk, hk_th, hk_ds, hk_ue,
           hc, hc_th, hc_ds, hc_ue,
           hs, hs_th, hs_ds, hs_ue,
@@ -209,7 +209,7 @@ for i = 2: n #BL march loop
            hkprev = hk
           else
            if(hk > hksep)  # .and. hk > hkprev) 
-#---------- Hk limit exceeded... switch to inverse mode for this point
+#---------- Hk limit exceeded... switch to inverse mode for θᵢs point
             direct = false
            end
           end
@@ -217,7 +217,7 @@ for i = 2: n #BL march loop
 #-------- set up 2-point differenced BL equation system
           aa, bb, rr = blsys(simi,lami,wake,direct, Mach, uinv[i],hksep,
                       x,b,rn,th,ds,ue, 
-                      h , h_th, h_ds,
+                      h , ∂h∂θ, ∂h∂δ,
                       hk, hk_th, hk_ds, hk_ue,
                       hc, hc_th, hc_ds, hc_ue,
                       hs, hs_th, hs_ds, hs_ue,
@@ -262,20 +262,20 @@ for i = 2: n #BL march loop
        end #newton iteration
 
 
-      uei[i] = ue
-      dsi[i] = ds
-      thi[i] = th
-      tsi[i] = hs*th
-      dci[i] = hc*th
+      uₑᵢ[i] = ue
+      δᵢ[i] = ds
+      θᵢ[i] = th
+      θ∗ᵢ[i] = hs*th
+      δᵢ⁺⁺[i] = hc*th
       cfi[i] = cf
       cdi[i] = di*hs/2.0
       cti[i] = 0.03 * 0.5*hs*((hk-1.0)/hk)^2
       hki[i] = hk
 
-      dib  = cdi[i]  *rhi[i]  *uei[i]^3   * (b  + 2.0*pi*ds *rn )
-      dibm = cdi[i-1]*rhi[i-1]*uei[i-1]^3 * (bm + 2.0*pi*dsm*rnm)
-      phi[i] = phi[i-1] + 0.5*(dib + dibm) * (x - xm)
-      mdi[i] = ue*ds*(b + 2.0*pi*ds)
+      dib  = cdi[i]  *ρᵢ[i]  *uₑᵢ[i]^3   * (b  + 2.0*π*ds *rn )
+      dibm = cdi[i-1]*ρᵢ[i-1]*uₑᵢ[i-1]^3 * (bm + 2.0*π*dsm*rnm)
+      ϕ[i] = ϕ[i-1] + 0.5*(dib + dibm) * (x - xm)
+      mdi[i] = ue*ds*(b + 2.0*π*ds)
 
 
       thm = th
@@ -284,8 +284,8 @@ for i = 2: n #BL march loop
       uem = ue
 
       hm = h
-      hm_thm = h_th
-      hm_dsm = h_ds
+      hm_thm = ∂h∂θ
+      hm_dsm = ∂h∂δ
 
       hkm = hk
       hkm_thm = hk_th
@@ -338,12 +338,12 @@ end # BL march loop
     
       for i = 1: n
         for j = 1: n-1
-          dx = xi[i] - 0.5*(xi[j+1]+xi[j])
+          dx = xᵢ[i] - 0.5*(xᵢ[j+1]+xᵢ[j])
 #         dm = mdi(j+1) - mdi[j]
-#         du = dm * qopi / (dx*abs(dx))
-#         duei = duei + du
-          uvis_mdi[i,j+1] = uvis_mdi[i,j+1] + qopi/(dx*abs(dx))
-          uvis_mdi[i,j  ] = uvis_mdi[i,j  ] - qopi/(dx*abs(dx))
+#         du = dm * π¼ / (dx*abs(dx))
+#         duₑᵢ = duₑᵢ + du
+          uvis_mdi[i,j+1] = uvis_mdi[i,j+1] + π¼/(dx*abs(dx))
+          uvis_mdi[i,j  ] = uvis_mdi[i,j  ] - π¼/(dx*abs(dx))
         end
       end
 
@@ -372,7 +372,7 @@ end # BL march loop
 
 
 #---- set current uvis corresponding to current mdi
-      dsi[1] = 0.
+      δᵢ[1] = 0.
       for i = 2: n
         uvis[i] = uinv[i]
         for j = 1: n
@@ -382,34 +382,34 @@ end # BL march loop
 
 #---- sweep downstream to set up BL equations
       for i = 2: n  # Set up BL loop
-        simi = xi[i-1] == 0.0
+        simi = xᵢ[i-1] == 0.0
 
 #c      lami = simi
         lami = false
 
         wake = i  > ite
 
-        x  = xi[i]
+        x  = xᵢ[i]
         b  = bi[i]
         rn = rni[i]
-        th = thi[i]
+        th = θᵢ[i]
         md = mdi[i]
-        ue = uei[i]
+        ue = uₑᵢ[i]
 
-        xm  = xi[i-1]
+        xm  = xᵢ[i-1]
         bm  = bi[i-1]
         rnm = rni[i-1]
-        thm = thi[i-1]
+        thm = θᵢ[i-1]
         mdm = mdi[i-1]
-        uem = uei[i-1]
+        uem = uₑᵢ[i-1]
 
-#c      md = ue*ds*(b + 2.0*pi*ds*rn)
+#c      md = ue*ds*(b + 2.0*π*ds*rn)
 
-#c      md/(ue*2*pi) = ds*(b/2pi + ds*rn)
-#c      0.5 rn ds^2 + (b/4pi)*ds - md/(4pi ue) = 0
-#c      ds = ( sqrt(bp^2 + 0.5*md/(pi*ue)) - bp ) / rn
-        bp = b * 0.25/pi
-        mpu = 0.5*md/(pi*ue)
+#c      md/(ue*2*π) = ds*(b/2π + ds*rn)
+#c      0.5 rn ds^2 + (b/4π)*ds - md/(4π ue) = 0
+#c      ds = ( sqrt(bp^2 + 0.5*md/(π*ue)) - bp ) / rn
+        bp = b * 0.25/π
+        mpu = 0.5*md/(π*ue)
         if(rn <= 1.0e-6) 
          ds    =  md/(ue*b)
          ds_md = 1.0/(ue*b)
@@ -430,8 +430,8 @@ end # BL march loop
           dsm_mdm = 1.0/(uem*bm)
           dsm_uem = -dsm/uem
          else
-          bpm = bm * 0.25/pi
-          mpum = 0.5*mdm/(pi*uem)
+          bpm = bm * 0.25/π
+          mpum = 0.5*mdm/(π*uem)
           dsm     =    (sqrt(bpm^2 + mpum) - bpm) / rnm
           dsm_mdm = 0.5/sqrt(bpm^2 + mpum) * ( mpum/mdm) / rnm
           dsm_uem = 0.5/sqrt(bpm^2 + mpum) * (-mpum/uem) / rnm
@@ -449,7 +449,7 @@ end # BL march loop
 #     &                 dim, dim_thm, dim_dsm, dim_uem )
 #        end
 
-        (h , h_th, h_ds,
+        (h , ∂h∂θ, ∂h∂δ,
         hk, hk_th, hk_ds, hk_ue,
         hc, hc_th, hc_ds, hc_ue,
         hs, hs_th, hs_ds, hs_ue,
@@ -459,7 +459,7 @@ end # BL march loop
             
         aa,bb,rr = blsys(simi,lami,wake,direct, Mach, uinv[i],hksep,
                       x,b,rn,th,ds,ue, 
-                      h , h_th, h_ds,
+                      h , ∂h∂θ, ∂h∂δ,
                       hk, hk_th, hk_ds, hk_ue,
                       hc, hc_th, hc_ds, hc_ue,
                       hs, hs_th, hs_ds, hs_ue,
@@ -513,8 +513,8 @@ end # BL march loop
         asys[ksys,lsys] = asys[ksys,lsys] + 1.0
 
 #------ also store dependent variables for returning
-        tsi[i] = hs*th
-        dci[i] = hc*th
+        θ∗ᵢ[i] = hs*th
+        δᵢ⁺⁺[i] = hc*th
         cfi[i] = cf
         cdi[i] = di*hs/2.0
         cti[i] = 0.03 * 0.5*hs*((hk-1.0)/hk)^2
@@ -522,8 +522,8 @@ end # BL march loop
 
 #------ set dependent i-1 variables for next interval
         hm = h
-        hm_thm = h_th
-        hm_dsm = h_ds
+        hm_thm = ∂h∂θ
+        hm_dsm = ∂h∂δ
   
         hkm = hk
         hkm_thm = hk_th
@@ -565,7 +565,7 @@ end # BL march loop
 #        k2 = 3*[i-1] + 2
 #        k3 = 3*[i-1] + 3
 #        write(*,'(1x,i4,3e12.4,3x,3g15.7)')
-#     &    i, rsys(k1),rsys(k2),rsys(k3), uei[i], uvis[i], uei[i]-uvis[i]
+#     &    i, rsys(k1),rsys(k2),rsys(k3), uₑᵢ[i], uvis[i], uₑᵢ[i]-uvis[i]
 #      end
 
 #---- solve Newton system
@@ -578,19 +578,19 @@ rsys = asys\rsys
         rlx = 1.0
         for i = 2: n
           b  = bi[i]
-          th = thi[i]
+          th = θᵢ[i]
           md = mdi[i]
-          ue = uei[i]
+          ue = uₑᵢ[i]
 
           kth = 3*(i-1) + 1
           kmd = 3*(i-1) + 2
           kue = 3*(i-1) + 3 
-          dthi[i] = -rsys[kth]
+          dθᵢ[i] = -rsys[kth]
           dmdi[i] = -rsys[kmd]
-          duei[i] = -rsys[kue]
+          duₑᵢ[i] = -rsys[kue]
 
-          bp = b * 0.25/pi
-          mpu = 0.5*md/(pi*ue)
+          bp = b * 0.25/π
+          mpu = 0.5*md/(π*ue)
           ds    =     sqrt(bp^2 + mpu) - bp
           ds_md = 0.5/sqrt(bp^2 + mpu) * ( mpu/md)
           ds_ue = 0.5/sqrt(bp^2 + mpu) * (-mpu/ue)
@@ -599,34 +599,34 @@ rsys = asys\rsys
 #          ds_md = 1.0/(b*ue)
 #          ds_ue = -ds/ue
 
-          ddsi[i] = ds_md*dmdi[i] + ds_ue*duei[i]
+          dδᵢ[i] = ds_md*dmdi[i] + ds_ue*duₑᵢ[i]
 
-          if(rlx*dthi[i] >  1.6*th) rlx =  1.6*th/dthi[i]; end 
-          if(rlx*dthi[i] < -0.6*th) rlx = -0.6*th/dthi[i]; end
-          if(rlx*ddsi[i] >  2.5*ds) rlx =  2.5*ds/ddsi[i]; end
-          if(rlx*ddsi[i] < -0.4*ds) rlx = -0.4*ds/ddsi[i]; end
-          if(rlx*duei[i] >  0.2*ue) rlx =  0.2*ue/duei[i]; end
-          if(rlx*duei[i] < -0.1*ue) rlx = -0.1*ue/duei[i]; end
+          if(rlx*dθᵢ[i] >  1.6*th) rlx =  1.6*th/dθᵢ[i]; end 
+          if(rlx*dθᵢ[i] < -0.6*th) rlx = -0.6*th/dθᵢ[i]; end
+          if(rlx*dδᵢ[i] >  2.5*ds) rlx =  2.5*ds/dδᵢ[i]; end
+          if(rlx*dδᵢ[i] < -0.4*ds) rlx = -0.4*ds/dδᵢ[i]; end
+          if(rlx*duₑᵢ[i] >  0.2*ue) rlx =  0.2*ue/duₑᵢ[i]; end
+          if(rlx*duₑᵢ[i] < -0.1*ue) rlx = -0.1*ue/duₑᵢ[i]; end
 
-          dmax = max( dmax , abs(dthi[i])/th , 
-	abs(ddsi[i])/ds , 
-	abs(duei[i])/ue   )
+          dmax = max( dmax , abs(dθᵢ[i])/th , 
+	abs(dδᵢ[i])/ds , 
+	abs(duₑᵢ[i])/ue   )
         end
 
 #------ perform Newton updated, with limiter if rlx < 1
         for i = 2: n
-          thi[i] = thi[i] + rlx*dthi[i]
-          dsi[i] = dsi[i] + rlx*ddsi[i]
-          uei[i] = uei[i] + rlx*duei[i]
-          mdi[i] = uei[i]*dsi[i]*(bi[i] + 2.0*pi*dsi[i]*rni[i])
+          θᵢ[i] = θᵢ[i] + rlx*dθᵢ[i]
+          δᵢ[i] = δᵢ[i] + rlx*dδᵢ[i]
+          uₑᵢ[i] = uₑᵢ[i] + rlx*duₑᵢ[i]
+          mdi[i] = uₑᵢ[i]*δᵢ[i]*(bi[i] + 2.0*π*δᵢ[i]*rni[i])
         end
 
 #        for i = 2: n
-#          thi[i] = thi[i] + rlx*dthi[i]
-#cc        dsi[i] = dsi[i] + rlx*ddsi[i]
-#          uei[i] = uei[i] + rlx*duei[i]
+#          θᵢ[i] = θᵢ[i] + rlx*dθᵢ[i]
+#cc        δᵢ[i] = δᵢ[i] + rlx*dδᵢ[i]
+#          uₑᵢ[i] = uₑᵢ[i] + rlx*duₑᵢ[i]
 #          mdi[i] = mdi[i] + rlx*dmdi[i]
-#          dsi[i] = mdi[i]/(bi[i]*uei[i])
+#          δᵢ[i] = mdi[i]/(bi[i]*uₑᵢ[i])
 #        end
 
 #        if(ipass==1) write(*,*)
@@ -640,16 +640,16 @@ rsys = asys\rsys
 
 
 #---- integrate running dissipation
-      phi[1] = 0.
+      ϕ[1] = 0.
       for i = 2: n
-        x  = xi[i]
-        xm = xi[i-1]
+        x  = xᵢ[i]
+        xm = xᵢ[i-1]
 
-        dib  = cdi[i]  *rhi[i]  *uei[i]^3   * (bi[i]   + 2.0*pi*dsi[i]  *rni[i])
-        dibm = cdi[i-1]*rhi[i-1]*uei[i-1]^3 * (bi[i-1] + 2.0*pi*dsi[i-1]*rni[i-1])
+        dib  = cdi[i]  *ρᵢ[i]  *uₑᵢ[i]^3   * (bi[i]   + 2.0*π*δᵢ[i]  *rni[i])
+        dibm = cdi[i-1]*ρᵢ[i-1]*uₑᵢ[i-1]^3 * (bi[i-1] + 2.0*π*δᵢ[i-1]*rni[i-1])
 
-        phi[i] = phi[i-1] + 0.5*(dib + dibm) * (x - xm)
+        ϕ[i] = ϕ[i-1] + 0.5*(dib + dibm) * (x - xm)
       end
 
-      return uei, dsi, thi, tsi, dci, cfi, cdi, cti, hki, phi
+      return uₑᵢ, δᵢ, θᵢ, θ∗ᵢ, δᵢ⁺⁺, cfi, cdi, cti, hki, ϕ
       end # blax
