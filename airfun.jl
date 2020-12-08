b44f0f2741447fb1264514296c2bc32c7faf71a0
Prashanth <prash@mit.edu>
2020-12-08 17:59:31 -0500

    Modified airfun and airtable to be slightly faster

--- airfun.jl
+++ airfun.jl
@@ -1,46 +1,28 @@
 using Dierckx
 
-function airfun(Ma, cl, τ, AMa, Acl, Aτ, ARe, A)
+function airfun(cl::Float64, τ::Float64, Ma::Float64, 
+        AMa::Vector{Float64}, Acl::Vector{Float64}, Aτ::Vector{Float64}, ARe::Float64,
+        A::Array{Float64},
+        ∂cdf_∂M::Array{Spline1D, 2}, ∂cdp_∂M::Array{Spline1D, 2}, ∂cm_∂M::Array{Spline1D, 2})
     
-    ∂cdf_∂M  = Array{Spline1D, 2}(undef, nAcl, nAtau)
-    ∂cdp_∂M  = Array{Spline1D, 2}(undef, nAcl, nAtau)
-     ∂cm_∂M  = Array{Spline1D, 2}(undef, nAcl, nAtau)
+    nAcl::Int  = length(Acl)
+    nAtau::Int = length(Aτ)
     
-    for k = 1:nAtau
-        for j = 1:nAcl
-            ∂cdf_∂M[j, k] = Spline1D(AMa, A[:,j,k,1], k = 3)
-            ∂cdp_∂M[j, k] = Spline1D(AMa, A[:,j,k,2], k = 3)
-             ∂cm_∂M[j, k] = Spline1D(AMa, A[:,j,k,3], k = 3)
-        end
-    end
     k = 1:nAtau
     j = 1:nAcl
     cdf_clτ = evaluate.(∂cdf_∂M[j,k], Ma) #cdf as a function of cl and τ
     cdp_clτ = evaluate.(∂cdp_∂M[j,k], Ma) #cdp as a function of cl and τ
      cm_clτ = evaluate.( ∂cm_∂M[j,k], Ma) #cm as a function of cl and τ
 
-    ∂cdf_∂cl = Array{Spline1D, 1}(undef, nAtau)
-    ∂cdp_∂cl = Array{Spline1D, 1}(undef, nAtau)
-     ∂cm_∂cl = Array{Spline1D, 1}(undef, nAtau)
+    ∂cdf_∂cl∂τ = Spline2D(Acl, Aτ, cdf_clτ; kx=3, ky=3, s=0.0)
+    ∂cdp_∂cl∂τ = Spline2D(Acl, Aτ, cdp_clτ; kx=3, ky=3, s=0.0)
+     ∂cm_∂cl∂τ = Spline2D(Acl, Aτ,  cm_clτ; kx=3, ky=3, s=0.0)
+
+    cdf = evaluate(∂cdf_∂cl∂τ, cl, τ)
+    cdp = evaluate(∂cdp_∂cl∂τ, cl, τ)
+     cm = evaluate( ∂cm_∂cl∂τ, cl, τ)
+    cdw = 0.0
     
-    for k = 1:nAtau
-         ∂cdf_∂cl[k] = Spline1D(Acl, cdf_clτ[:,k], k = 3)
-         ∂cdp_∂cl[k] = Spline1D(Acl, cdp_clτ[:,k], k = 3)
-          ∂cm_∂cl[k] = Spline1D(Acl,  cm_clτ[:,k], k = 3)
-    end
-
-    cdf_τ = evaluate.(∂cdf_∂cl[k], cl)
-    cdp_τ = evaluate.(∂cdp_∂cl[k], cl)
-     cm_τ = evaluate.( ∂cm_∂cl[k], cl)
-
-    ∂cdf_∂τ = Spline1D(Atau, cdf_τ, k = 3)
-    ∂cdp_∂τ = Spline1D(Atau, cdp_τ, k = 3)
-     ∂cm_∂τ = Spline1D(Atau,  cm_τ, k = 3)
-
-    cdf = evaluate(∂cdf_∂τ, τ)
-    cdp = evaluate(∂cdp_∂τ, τ)
-     cm = evaluate( ∂cm_∂τ, τ)
-
-    return cdf, cdp, cm
+    return cdf, cdp, cdw,  cm
 
 end #function airfun
