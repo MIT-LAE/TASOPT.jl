b6b1331f3fe460d3c6e73ddf6eeaccb4643a200f
szahid <szahid@mit.edu>
2020-12-14 20:23:06 +0500

    added comments

--- tankWmech.jl
+++ tankWmech.jl
@@ -2,22 +2,22 @@
 tankWmech calculates weight of the tank that holds the LH2
 
 Inputs:
-
+NOTE: Everything is in SI units.
 -LD is L/D of airplane
--xshell1, xshell2 are start and end coordinates of tank
--gee is gravitational acceleration
--Rfuse is fuselage radius, dRfuse is the subtraction factor that accounts for flatness of fuselage at bottom
+-xshell1, xshell2 are start and end coordinates of tank (in meters)
+-gee is gravitational acceleration (m2/s)
+-Rfuse is fuselage radius (m), dRfuse (m) is the subtraction factor that accounts for flatness of fuselage at bottom
 -wfb, nfb are parameters for multiple-bubble configuration
--m_airplane is airplane mass
--R is specified range for a given mission
--lcv is lower calorific value of fuel
+-m_airplane is airplane mass (kg)
+-R (m) is specified range for a given mission
+-lcv (J/kg) is lower calorific value of fuel
 -eta is overall efficiency of gas turbine/turboelectric powertrain etc.
 -sigskin, rhoskin are material properties
 
 Outputs:
 
-- Wtank: tank weight
-- tankVol: tank volume
+- Wtank: tank weight (N)
+- Wfuel: fuel weight (N)
 """
 function tankWmech(gee, rhoFuel,
                       fstring,fframe,ffadd,deltap,
@@ -27,11 +27,11 @@ function tankWmech(gee, rhoFuel,
                       m_airplane, R, lcv, eta, LD)
 
 #--- effective pressure-vessel length
-      lshell = xshell2 - xshell1
+      lshell = xshell2 - xshell1 #length of pressure vessel
 
 #--- Calculate Wfuel
-      m_st = m_airplane * exp(R * gee / (lcv * eta * LD))
-      Wfuel = (m_st - m_airplane) * gee
+      m_st = m_airplane * exp(R * gee / (lcv * eta * LD)) #mass at start of cruise
+      Wfuel = (m_st - m_airplane) * gee #fuel weight for the required LH2
 
 #--- fuselage cross-section geometric parameters
       wfblim = max( min( wfb , Rfuse ) , 0.0 )
@@ -59,7 +59,7 @@ function tankWmech(gee, rhoFuel,
 #--- insulation weight
       Winsul = Wppinsul*(1.1*pi+2.0*thetafb)*Rfuse*lshell
 
-#--- overall tank weight and moment
+#--- overall tank weight
       Wtank = Wtank + Winsul + Wfuel
 
 #--- pressurized tank volume
