cce54d80e467be13091f3a4c76ff4ab2d7947757
Prashanth <prash@mit.edu>
2020-12-26 13:05:35 -0500

    prettify wingsc

--- wingsc.jl
+++ wingsc.jl
@@ -2,19 +2,19 @@
      Sets wing area, span, root chord 
      to be consistent with q,CL,weight,AR
 """
-      function wingsc(W,CL,qinf,AR,etasi,bo,lambdat,lambdas)
+      function wingsc(W,CL,qinf,AR,ηsi,bo,λt,λs)
 
       S = W/(qinf*CL)
       b = sqrt(S*AR)
 
-      bs = max( b*etasi , bo )
+      bs = max( b*ηsi , bo )
 
-      etao = bo/b
-      etas = bs/b
+      ηo = bo/b
+      ηs = bs/b
 
-      Kc = etao +
-	 0.5*(1.0    +lambdas)*(etas-etao) +
-	 0.5*(lambdas+lambdat)*(1.0 -etas)
+      Kc = ηo +
+	 0.5*(1.0    +λs)*(ηs-ηo) +
+	 0.5*(λs+λt)*(1.0 -ηs)
 
       co = S/(Kc*b)
 
@@ -26,16 +26,16 @@
 #  Sets wing area, AR, root chord 
 #  to be consistent with q,CL,weight,span
 #"""
-#      function wingAc(W,CL,qinf,b,etasi,bo,bs,lambdat,lambdas)
+#      function wingAc(W,CL,qinf,b,ηsi,bo,bs,λt,λs)
 #
 #      S = W/(qinf*CL)
 #
-#      etao = bo/b
-#      etas = bs/b
+#      ηo = bo/b
+#      ηs = bs/b
 #
-#      Kc = etao +
-#	 0.5*(1.0    +lambdas)*(etas-etao) +
-#	 0.5*(lambdas+lambdat)*(1.0 -etas)
+#      Kc = ηo +
+#	 0.5*(1.0    +λs)*(ηs-ηo) +
+#	 0.5*(λs+λt)*(1.0 -ηs)
 #
 #      co = S/(Kc*b)
 #
