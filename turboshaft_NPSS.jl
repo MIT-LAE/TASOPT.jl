701c8ccc8c2721a7d093118766806047206dd1dc
Prashanth <prash@mit.edu>
2021-01-03 00:07:26 -0500

    Update julia turboshaft engine functions to write input files

--- turboshaft_NPSS.jl
+++ turboshaft_NPSS.jl
@@ -8,3 +8,23 @@ function turboshaft_NPSS(dir, bat_file)
     run(`cmd /c cd $dir '&&' $bat_file `)
     return nothing
 end
+
+"""
+Writes an input file for NPSS
+"""
+function NPSS_input(SHP_dmd, Tt41, LPC_PR, HPC_PR ; LHV = 120e6, file_name = "NPSS_Turboshaft/EngineInputs.inp")
+
+    open(file_name, "w") do io
+        println(io, "// Targets")
+        println(io, "real SHP_dmd = ", SHP_dmd, ";" )
+        println(io, "real Tt41    = ", Tt41, ";" )
+
+        println(io, "// LHV based on fuel")
+        println(io, "Eng.FusEng.LHV = ", LHV/430, ";")
+
+        println(io, "// Design parameters")
+        println(io, "Eng.CmpL.PRdes = ", LPC_PR, ";")
+        println(io, "Eng.CmpH.PRdes = ", HPC_PR, ";")
+    end
+
+end
