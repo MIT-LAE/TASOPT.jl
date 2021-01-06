a0944c9826b1dbb575eeab9d70deab0b1d30abc4
Prashanth <prash@mit.edu>
2021-01-06 17:46:12 -0500

    Update propsys to give eta prop also from ducted fan

--- propsys.jl
+++ propsys.jl
@@ -9,15 +9,24 @@ Inputs:
 - Fn     : Design Thurst reqired 
 - π_fan  : Fan pressure ratio
 
+Outputs:
+- Dfan        : diameter of fan in [m]
+- Fan_power   : Required fan shaft power [kW]
+- Torque_fan  : Required fan torque [N*m]
+- N_fan       : Fan speed [RPM]
+- eta_prop    : propulsive eff
+- MapScalars  : Fan map scalars for off-des calcs
+- NozArea     : Fan nozzle area for off-des calcs [in^2] in inches so can easily pass back to NPSS
+
 """
 function DuctedFan(alt_in::Float64, MN_in::Float64,  Fn::Float64, π_fan::Float64,)
-    Design = true
+    
     NPSS_Fan_input(alt_in, MN_in, Fn, π_fan)
     NPSS_run("NPSS_Turboshaft/", "Fan.bat")
 
     include("NPSS_Turboshaft/Fan.output")
 
-    return Design, Dfan, Fan_power, Torque_fan, N_fan, MapScalars, NozArea
+    return Dfan, Fan_power, Torque_fan, N_fan, eta_prop, MapScalars, NozArea
 
 end
 """
@@ -32,14 +41,21 @@ Inputs:
 - Map Scalars  : Fan map scalars for off-des
 - NozArea      : Nozzle area for off-des
 
+Outputs:
+- Fan_power   : Required fan shaft power [kW]
+- Torque_fan  : Required fan torque [N*m]
+- N_fan       : Fan speed [RPM]
+- eta_prop    : propulsive eff
+
+
 """
 function DuctedFan(alt_in::Float64, MN_in::Float64,  Fn::Float64, MapScalars::Array{Float64, 1}, NozArea::Float64)
-    Design = false
+    
     NPSS_Fan_input(alt_in, MN_in, Fn, MapScalars, NozArea)
     NPSS_run("NPSS_Turboshaft/", "Fan.bat")
 
     include("NPSS_Turboshaft/Fan.output")
 
-    return Design, Dfan, Fan_power, Torque_fan, N_fan
+    return Fan_power, Torque_fan, N_fan, eta_prop
 
 end
