88d19a0f47e0a019927aeae139c04500b1ed8dd2
szahid <szahid@mit.edu>
2021-01-13 14:43:00 +0500

    added parameters as inputs according to energies literature

---
+++ weight_routine_tank.jl
@@ -0,0 +1,6 @@
+(gee, rhoFuel, deltap, Rfuse, dRfuse, hconvgas, h_LH2, Tfuel, Tair,
+                      h_v, t_cond, k, hconvair, time_flight, fstring,ffadd,
+                      wfb, nfweb, sigskin, rho_insul, rhoskin, Wfuel, threshold_percent, mode) =
+                      (9.81, 67.3, 51325, 3.1, 0, 25, 50, 20, 268.15, 446000,
+                      [0.01, 1.524e-5, 0.01, 1.524e-5, 1.57e-2], [20e-3, 20e-3, 20e-3, 20e-3, 20e-3],
+                      100, 18000, 0.34, 0.2, 0, 0, 290e6, [35.24, 14764, 35.24, 14764, 83], 2840, 2000, 0.1, 1)
