e2020dfcb6e581fd5b42e3d62bf45df9bc69fdcf
Prashanth <prash@mit.edu>
2021-02-02 17:54:19 -0500

    Modified tank codes and update LH2 props

--- hydrogen.jl
+++ hydrogen.jl
@@ -1,13 +1,18 @@
-# Properties of hydrogen at saturation
+# Properties of hydrogen at saturation from NIST
+# P is in [atm] 
 # Saturated liquid
-    ρl(P) = 0.3175*P^2 - 4.1121*P + 74.544 # Fit to above data P is in atm!
-    ul(P) = -2.8443*P^2 + 34.84P  - 33.044
-    hl(P) = -2.7899*P^2 + 36.244P - 33.072
+    ρl(P) = 0.3175*P^2 - 4.1121*P + 74.544 # returns in kg/m3
+    ul(P) = -2.8443*P^2 + 34.84P  - 33.044 # returns in kJ/kg
+    hl(P) = -2.7899*P^2 + 36.244P - 33.072 # returns in kJ/kg
 # Saturated vapor
-    ρg(P) =  9.0e-5*P^2 + 1.1838P + 0.1589
-    ug(P) = -2.3398*P^2 + 13.435P + 359.02
-    hg(P) = -3.7721*P^2 + 22.094P + 427.65
+    ρg(P) =  9.0e-5*P^2 + 1.1838P + 0.1589 # returns in kg/m3
+    ug(P) = -2.3398*P^2 + 13.435P + 359.02 # returns in kJ/kg
+    hg(P) = -3.7721*P^2 + 22.094P + 427.65 # returns in kJ/kg
 
+# mixed
+    umix(x,P) = x*ug(P) + (1-x)*ul(P)
+    # want in kJ/kg / kPa so ÷ by 101.325
+    u′mix(x,P) = (x*(-2.3398*2*P + 13.435) +(1-x)*(-2.8443*P*2 + 34.84))/101.325 
 
 # Let yg be the ullage volume fraction - 3% ullage ⟹ a volume fill factor of 97%
 ρmix(yg,P) = ρl(P)*(1-yg) + ρg(P)*yg
@@ -20,9 +25,11 @@ function φ(P,δP,yg)
     ρ = 0.5*(ρmix_qual(x,P) +  ρmix_qual(x, P+δP))
     x = ( 1/ρ - 1/ρl(P+δP) ) / ( 1/ρg(P+δP) - 1/ρl(P+δP) )
 
-    uᵢ   = x*ug(P)    + (1-x)*ul(P)
-    uᵢ₊₁ = x*ug(P+δP) + (1-x)*ul(P+δP)
-    φ = (ρ*(uᵢ₊₁ - uᵢ)/(δP*101.325))^-1
+    # uᵢ   = x*ug(P)    + (1-x)*ul(P)
+    # uᵢ₊₁ = x*ug(P+δP) + (1-x)*ul(P+δP)
+    # ∂u_∂P = (uᵢ₊₁ - uᵢ)/(δP*101.325) 
+    # φ = (ρ*∂u_∂P)^-1
+    φ = (ρ*u′mix(x,P+δP))^-1
 
     return φ
 end
@@ -41,16 +48,31 @@ end
 
 dPdt_sp(Q, V, P, dP, yg) = φ(P, dP, yg)/V * Q
 
-time = 0:1:10
+time = 0:0.5:10
 P = zeros(length(time))
-P[1] = 101.325
+y = 0.8
+P[1] = 1.5
+Vol = 200.0
+fac = 10.0
+q = (1.3*(298+10-20)/0.225)
 for (i,t) in enumerate(time)
     if(i<length(time))
-        P[i+1] = P[i] + dPdt_sp(200, 52, P[i]/101.325, 0.01, 0.5)*24*3600(time[i+1] - time[i])/1000
+        P[i+1] = P[i] + fac*dPdt_sp(q, Vol, P[i]/101.325, 0.01, y)*3600(time[i+1] - time[i])/101325
     end
 end
-using PyPlot
-plot(time,P)
+
+println("P = np.array($P)")
+println("t = $time")
+println("q = $q")
+println("y = $y")
+# using PyPlot
+# pygui(true)
+# plt = plot(time,P, label = "y = 0.1")
+# q = round(q/1000;digits = 2)
+# title("P[atm] vs t[hrs] for q = $q kW")
+# legend()
+# grid("on")
+
 
 # mdot = [0.617606, 0.52916, 0.437406, 0.359918, 0.295704, 0.291292, 0.271806]
 # time = [174.10271765493297, 340.5402718139433, 584.5015556778529, 1155.4673259995732, 1155.4673259995732, 22678.920538191625, 22678.920538191625]
