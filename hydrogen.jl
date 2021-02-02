a3d71b8634ee165194fec89d47aafaed99042604
Prashanth <prash@mit.edu>
2021-02-02 00:29:58 -0500

    update H2 props

--- hydrogen.jl
+++ hydrogen.jl
@@ -11,12 +11,14 @@
 
 # Let yg be the ullage volume fraction - 3% ullage ⟹ a volume fill factor of 97%
 ρmix(yg,P) = ρl(P)*(1-yg) + ρg(P)*yg
+ρmix_qual(x,P) = (x/ρg(P) + (1-x)/ρl(P))^-1
 
 quality(yg, P) = 1/( 1 + (ρl(P)/ρg(P)) * (1-yg)/yg )
 
 function φ(P,δP,yg)
-    ρ = 0.5*(ρmix(yg, P) + ρmix(yg, P+δP))
-    x = ( 1/ρ - 1/ρl(P) ) / ( 1/ρg(P) - 1/ρl(P) )
+    x = quality(yg, P)
+    ρ = 0.5*(ρmix_qual(x,P) +  ρmix_qual(x, P+δP))
+    x = ( 1/ρ - 1/ρl(P+δP) ) / ( 1/ρg(P+δP) - 1/ρl(P+δP) )
 
     uᵢ   = x*ug(P)    + (1-x)*ul(P)
     uᵢ₊₁ = x*ug(P+δP) + (1-x)*ul(P+δP)
@@ -31,26 +33,40 @@ function dPdt_LH2(ΔT, mdot_out, P, yg)
     Vfuel = 200.0
     ρbar = ρg(P)/(ρg(P) - ρl(P))
 
-    dPdt = φ(P, 0.01, yg)/Vfuel * (dqdt - mdot_out*(hl(P)-hg(P))*(ρbar))
+    dPdt = φ(P, 0.1, yg)/Vfuel * (dqdt - mdot_out*(hl(P)-hg(P))*1000*(ρbar))
 
-    return dPdt
+    return dPdt/101325
 end
 
-mdot = [0.617606, 0.52916, 0.437406, 0.359918, 0.295704, 0.291292, 0.271806]
-time = [174.10271765493297, 340.5402718139433, 584.5015556778529, 1155.4673259995732, 1155.4673259995732, 22678.920538191625, 22678.920538191625]
 
-P = zero(mdot)
-yg = zero(mdot)
-P[1] = 1.2
-yg[1] = 0.1
-ΔT = 226.0 - 20.0
+dPdt_sp(Q, V, P, dP, yg) = φ(P, dP, yg)/V * Q
+
+time = 0:1:10
+P = zeros(length(time))
+P[1] = 101.325
 for (i,t) in enumerate(time)
-    
-    if(i<7)
-        yg[i+1] = yg[i] + mdot[i]/ρmix(yg[i], P[i])
-        
-        P[i+1] = P[i] + dPdt_LH2(ΔT, mdot[i], P[i], yg[i])*(time[i+1] - time[i])
-        println(dPdt_LH2(ΔT, mdot[i], P[i], yg[i]))
+    if(i<length(time))
+        P[i+1] = P[i] + dPdt_sp(200, 52, P[i]/101.325, 0.01, 0.5)*24*3600(time[i+1] - time[i])/1000
     end
 end
-P
+using PyPlot
+plot(time,P)
+
+# mdot = [0.617606, 0.52916, 0.437406, 0.359918, 0.295704, 0.291292, 0.271806]
+# time = [174.10271765493297, 340.5402718139433, 584.5015556778529, 1155.4673259995732, 1155.4673259995732, 22678.920538191625, 22678.920538191625]
+
+# P = zero(mdot)
+# yg = zero(mdot)
+# P[1] = 1.2
+# yg[1] = 0.1
+# ΔT = 226.0 - 20.0
+# for (i,t) in enumerate(time)
+    
+#     if(i<7)
+#         yg[i+1] = yg[i] + mdot[i]/ρmix(yg[i], P[i])
+        
+#         P[i+1] = P[i] + dPdt_LH2(ΔT, mdot[i], P[i], yg[i])*(time[i+1] - time[i])
+#         println(dPdt_LH2(ΔT, mdot[i], P[i], yg[i]))
+#     end
+# end
+# P
