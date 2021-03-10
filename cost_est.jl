d386ece886d480f98a5f6156f65f13e2a3d23136
jmabel <jmabel@mit.edu>
2021-03-10 12:00:57 -0500

    implement Markish cost adjustments

--- cost_est.jl
+++ cost_est.jl
@@ -15,14 +15,13 @@ cost    total program cost estimate
 """
 
 function CostEst(parg, pare, prod_Q)
-    Wempty  = (parg[igWMTO] - parg[igWfuel] - parg[igWpay]) / 9.81 #empty weight (kg)
-    # TODO - add adjustments based on Markish for component-level weight differences (compared to conv. aircraft)
+    Wmod = (1.49*parg[igWfuse] + 2.42*(parg[igWhtail]+parg[igWvtail]) + 0.82*parg[igWwing] + 0.40*(parg[igWtesys])
+    + 0.12*parg[igWMTO]*(parg[igflgnose]+parg[igflgmain]) + 1.59*(parg[igWMTO]*parg[igfhpesys]+parg[igWftank])) / 9.81
+    # a modified OEW (in kg) that assigns more weight to systems with a higher cost per pound
+    # based on methods using in Markish (2000), with componenent weights determined from data for a 777-200
     Vmax = maximum(pare[ieu0,:]) * 3.6 #maximum velocity (km/h)
-    Neng = parg[igneng] #number of engines
     Npax = 180 #number of pax (TODO - make a parameter)
-    T4 = pare[ieTt4] * 5/9 #turbine inlet temperature (K)
-    Fmax = maximum(pare[ieFe,:]) / Neng / 1000 #maximum thrust per eninge (kN)
-    Mmax = maximum(pare[ieM0,:]) #maximum flight Mach number
+
 
     PPI = 242.8/144.8 #aerospace producer price index ratio from 1999 to 2020 (from US BLS)
     FTA = 6 #number of flight test aircraft (assume upper end due to novelty of propulsion system tech)
@@ -32,24 +31,22 @@ function CostEst(parg, pare, prod_Q)
     RQ = 81 #quality control wrap rate
     RM = 73 #manufacturing wrap rate
 
-    HE = 5.18*Wempty^0.777*Vmax^0.894*prod_Q^0.163 #engineering hours
-    HT = 7.22*Wempty^0.777*Vmax^0.696*prod_Q^0.263 #tooling hours
-    HM = 10.5*Wempty^0.82*Vmax^0.484*prod_Q^0.641 #manufacturing hours
+    HE = 5.18*Wmod^0.777*Vmax^0.894*prod_Q^0.163 #engineering hours
+    HT = 7.22*Wmod^0.777*Vmax^0.696*prod_Q^0.263 #tooling hours
+    HM = 10.5*Wmod^0.82*Vmax^0.484*prod_Q^0.641 #manufacturing hours
     HQ = 0.133*HM #quality control hours
 
-    CD = 48.7*Wempty^0.630*Vmax^1.3 #development support cost_est
-    CF = 1408*Wempty^0.325*Vmax^0.822*FTA^1.21 #flight test cost
-    CM = 22.6*Wempty^0.921*Vmax^0.621*prod_Q^0.799 #manufacturing materials cost (assumes Al construction)
+    CD = 48.7*Wmod^0.630*Vmax^1.3 #development support cost_est
+    CF = 1408*Wmod^0.325*Vmax^0.822*FTA^1.21 #flight test cost
+    CM = 22.6*Wmod^0.921*Vmax^0.621*prod_Q^0.799 #manufacturing materials cost (assumes Al construction)
 
-    CE = 2251*(9.66*Fmax + 243.25*Mmax + 1.74*T4 - 2228)*1.2 #engine cost 
-    #TODO - adjust to incorporate electric elements of propulsion system
     cA = 4/3 #adjustment factor, such that avionics costs are around 25% of flyaway cost
     CI = 2500*Npax #interior cost (e.g. seats, floors, etc.)
     # source: Raymer (2006), Collinson (2002)
 
-    #TODO - other adjustments specific to H2 (e.g. fuel system)
+    #TODO - other adjustments specific to H2 (e.g. fuel system, propulsion system)
 
-    cost = PPI*(HE*RE + HT*RT + HM*RM + HQ*RQ + CD + CF + CM + CI + Neng*CE)*cA
+    cost = PPI*(HE*RE + HT*RT + HM*RM + HQ*RQ + CD + CF + CM + CI)*cA
     # returns total program cost - divide by prod_Q to yield cost per aircraft
 
     return cost
