2989e3f59defd49ada29913e8519ab9d062c34e1
Prashanth <prash@mit.edu>
2021-04-28 16:30:56 -0400

    Need4speed - remove bound checks in airtable and airfun

--- airtable.jl
+++ airtable.jl
@@ -15,13 +15,13 @@ function airtable(fname)
     Aτ  = zeros(nAτ)
     A   = zeros(nAMa, nAcl, nAτ, nAfun)
 
-    for i = 1:nAMa
+    @inbounds for  i = 1:nAMa
        AMa[i] = parse(Float64, readline(f))
     end
-    for i=1:nAcl
+    @inbounds for  i=1:nAcl
         Acl[i] = parse(Float64, readline(f))
     end
-    for i=1:nAτ
+    @inbounds for  i=1:nAτ
         Aτ[i] = parse(Float64, readline(f))
     end
 
@@ -29,9 +29,9 @@ function airtable(fname)
     ARe = parse(Float64, readline(f))
 
     #Read in airfoil data
-    for k = 1:nAτ
-        for j = 1:nAcl
-            for i = 1:nAMa
+    @inbounds for  k = 1:nAτ
+        @inbounds for  j = 1:nAcl
+            @inbounds for  i = 1:nAMa
                     A[i,j,k,:] = [parse(Float64,ss) for ss in split(readline(f))]
             end
         end
@@ -45,8 +45,8 @@ function airtable(fname)
     ∂cdp_∂M  = Array{Spline1D, 2}(undef, nAcl, nAτ)
      ∂cm_∂M  = Array{Spline1D, 2}(undef, nAcl, nAτ)
 
-    for k = 1:nAτ
-        for j = 1:nAcl
+    @inbounds for  k = 1:nAτ
+        @inbounds for  j = 1:nAcl
             ∂cdf_∂M[j, k] = Spline1D(AMa, A[:,j,k,1], k = 3)
             ∂cdp_∂M[j, k] = Spline1D(AMa, A[:,j,k,2], k = 3)
              ∂cm_∂M[j, k] = Spline1D(AMa, A[:,j,k,3], k = 3)
