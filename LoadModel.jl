127fdcfa5f51967407a18dc0a3500c996075992d
Prashanth <prash@mit.edu>
2021-10-12 14:53:34 -0400

    modify load model

--- LoadModel.jl
+++ LoadModel.jl
@@ -1,8 +1,10 @@
 # Runs existing models
-function load_aircraft(name, dir, saveOD)
+function load_aircraft(name, dir, saveOD; run = false)
     include("index.inc")
     include(dir *"/"* name)
     fig = plot_details(parg, pari, para, parm)
-    run_wsize(35, 0, false, true, saveOD)
+    if run
+        run_wsize(35, 0, false, true, saveOD)
+    end
 
 end
