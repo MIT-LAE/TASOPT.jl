ce697dc0f76e2cca0254035c9259fb8d5fd2e573
Prashanth <prash@mit.edu>
2022-08-13 16:59:00 -0400

    Include Fuse BL calcs into aerodynamics:
    - converted atmos to module
    - import into aerodynamics and tasopt

--- src/aero/aero.jl
+++ src/aero/aero.jl
@@ -1,17 +1,35 @@
 module aerodynamics
 
-export airfun, airtable, trefftz1, cdsum!, cfturb, cditrp, surfcd, surfcd2, surfcm, wingsc, wingpo, wingcl
+using StaticArrays
+using ..atmosphere
 
-include("../misc/index.jl")
-print(igAR)
+export airfun, airtable, trefftz1, cdsum!, cfturb, cditrp, surfcd, surfcd2, surfcm, wingsc, wingpo, wingcl,
+fusebl!
+
+#include index to access arrays
+include("../misc/index.inc")
+
 include("../../utils/spline.jl")
+
+# Aerofoil calculations
 include("airtable2.jl")
 include("airfun2.jl")
-include("trefftz.jl")
-include("cdsum.jl")
 include("surfcd.jl")
 include("surfcm.jl")
 include("wingpo.jl")
 include("wingsc.jl")
+
+# Fuselage IBLT calculations
+include("fusebl.jl")
+include("axisol.jl")
+include("blax.jl")
+include("blsys.jl")
+
+# Trefftz plane CDi calcs
+include("trefftz.jl")
+
+# Total CD calculations 
+include("cdsum.jl")
+
 
 end
