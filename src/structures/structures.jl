ff940ca1bec3decefa08650e95bf2f4bfc8bdcea
Prashanth <prash@mit.edu>
2022-10-19 12:16:41 -0400

    Modularize struct and add to tasopt

---
+++ src/structures/structures.jl
@@ -0,0 +1,23 @@
+"""
+`structures` is a module that contains all the structural modules
+required to size an aircraft
+"""
+
+module structures
+
+export surfw, surfdx, fusew, tailpo, tanksize
+
+#include fuselage sizing
+include("fuseW.jl")
+
+#include sizing of surfaces
+include("surfdx.jl")
+include("surfw.jl")
+include("tailpo.jl")
+
+#Hydrogen tank related code
+include("tankWmech.jl")
+include("tankWthermal.jl")
+include("tanksize.jl")
+
+end
--- tasopt.jl
+++ tasopt.jl
@@ -1,5 +1,7 @@
 
 include("atmos/atmos.jl")
 include("src/aero/aero.jl")
+include("src/structures/structures.jl")
 using .atmosphere
 using .aerodynamics
+using .structures
