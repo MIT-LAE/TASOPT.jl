81289b691a25837a978e106407fcd8ee9ea3b679
Prashanth <prash@mit.edu>
2022-11-21 09:33:41 -0500

    Rearrange folder + clean up example run file

--- run/example_run.jl
+++ run/example_run.jl
@@ -2,125 +2,20 @@ using Pkg
 Pkg.activate("../")
 Pkg.instantiate()
 
-using Base: SignedMultiplicativeInverse
-using NLopt: G_MLSL_LDS, GN_MLSL_LDS, GN_CRS2_LM, GN_DIRECT_L
-
-using BenchmarkTools
-using Printf
-
-using StaticArrays
-using Profile, UnicodePlots
-using PyPlot
-using Dates
-
-include("../src/misc/index.inc")
-include("../src/misc/constants.jl")
-
-include("../engine/PT.inc")
+# 1) Load TASOPT
 include("../tasopt.jl")
 
-include("../balance/balance.jl")
-
-# include("../engine/PMSM.jl")  # Motor/generator functions
-# include("../engine/NPSS_functions.jl") # NPSS functions
-
-
-include("../fuel/hydrogen.jl")
-
-include("../sizing/wsize.jl")
-include("../src/mission/mission.jl")
-
+# 2) Include input file for desired aircraft/
+#  can also be a saved model
 include("input.jl")
-include("../IO/outputs.jl")
-include("../IO/savemodel.jl")
-include("../cost/cost_est.jl")
-include("../utils/printBADA.jl")
-include("../src/mission/odperformance.jl")
-include("../contrail/AircraftDeck.jl")
-include("../src/mission/LTO.jl")
 
+# 3) Size aircraft
+time_wsize = @elapsed size_aircraft(35, 0, false, true, saveOD)
 
-TSL, pSL, ρSL, aSL, μSL = atmos(0.0)
-RSL =  pSL/(ρSL * TSL)
-
-
-# temporary
-μAir = 1.78e-5 
-ρAir = ρSL
-
-time_writing = 0.0
-time_run_NPSS = 0.0
-f = open("temp.results", "w")
-# f = stdout
-
-initwgt = 0
-saveOD = false
-track_fig = nothing
-opt_iter_counter = 0
-function run_wsize(iter, initwgt, Ldebug, printiter, saveOD)
-    global time_writing = 0.0
-    global time_run_NPSS = 0.0
-    parpt[ipt_time_NPSS] = 0.0
-    parpt[ipt_calls_NPSS] = 0
-    global opt_iter_counter += 1
-
-    Ldebug && println("Max weight iterations = $iter")
-    wsize(pari, parg, parm, view(para,:,:,1), view(pare, :,:,1),
-                    iter, 0.5, 0.9, 0.5, initwgt, 1, 1, Ldebug, printiter, saveOD)
-
-    ## Write outputs to io stream f
-    printoutput = false
-    if printoutput
-        println(f, "AR  = $(parg[igAR])\n")
-        println(f, "CL  = $(para[iaCL, ipcruise1])\n")
-        println(f, "Alt  = $(para[iaalt, ipcruise1])\n")
-        @printf(f,"--------------------\n")
-        println(f, "Fe    = np.array(",pare[ieFe,:], ")")
-
-        println(f, "ηmot     = np.array(", pare[ieemot    , :], ")")
-        println(f, "ηinv     = np.array(", pare[ieeinv    , :], ")")
-        println(f, "ηcable   = np.array(", pare[ieecable  , :], ")")
-        println(f, "ηgen     = np.array(", pare[ieegen    , :], ")")
-        println(f, "ηthermal = np.array(", pare[ieethermal, :], ")")
-
-        println(f, "Hmot     = np.array(", pare[ieHrejmot , :], ")")
-        println(f, "Hinv     = np.array(", pare[ieHrejinv , :], ")")
-        println(f, "Hcable   = np.array(", pare[ieHrejcab , :], ")")
-        println(f, "Hgen     = np.array(", pare[ieHrejgen , :], ")")
-        println(f, "Htot     = np.array(", pare[ieHrejtot , :], ")")
-        println(f, "EINOx1 = np.array(", pare[ieEINOx1, :], ")")
-        println(f, "EINOx2 = np.array(", pare[ieEINOx2, :], ")")
-        println(f, "FAR = np.array(", pare[ieFAR, :], ")")
-
-        println(f, "h     = np.array(",para[iaalt,:],")")
-        println(f, "R     = np.array(",para[iaRange,:],")")
-        println(f, "deNOx = np.array(",pare[iedeNOx, :],")")
-        println(f, "fracW = np.array(",para[iafracW, :],")")
-        println(f, "mdotf = np.array(",pare[iemdotf, :],")")
-        println(f, "mdotH2O = np.array(",pare[iemdotf, :].* 9.0,")")
-        println(f, "Ptank = np.array(",pare[iePLH2, :],")")
-        println(f, "CL = np.array(",para[iaCL, :],")")
-        println(f, "CD = np.array(",para[iaCD, :],")")
-        println(f, "CLh = np.array(",para[iaCLh, :],")\n")
-
-
-        @printf(f, "\nPFEI = %.5f J/Nm\n", parm[imPFEI])
-
-        weight_buildup(parg, io = f)
-        aero(parg, para, io = f)
-        geometry(parg, io = f)
-    end
-
-    # global track_fig = stickfig(parg, pari,  parm; ax = track_fig)
-    if (opt_iter_counter%5 == 0) || (opt_iter_counter == 1)
-        global track_fig = plot_details(parg, pari, para, parm; ax = track_fig)
-    end
-end
-
-close(f)
-time_wsize = @elapsed run_wsize(35, 0, false, true, saveOD)
-# @profview run_wsize(25, 0, false, true)
-
+# 4) Visualize outputs
+# Output resulting geometry of aircraft
 geometry(parg)
+
+# Show weight buildup of the aircraft
 weight_buildup(parg)
 
--- tasopt.jl
+++ tasopt.jl
@@ -2,12 +2,85 @@
 TASOPT
 """
 
+# Add basic pacakges required by TASOPT
+using Base: SignedMultiplicativeInverse
+using NLopt: G_MLSL_LDS, GN_MLSL_LDS, GN_CRS2_LM, GN_DIRECT_L
+
+using BenchmarkTools
+using Printf
+
+using StaticArrays
+using Profile, UnicodePlots
+using PyPlot
+using Dates
+
+# Constants and array indices
 include("src/misc/constants.jl")
+include("src/misc/index.inc")
+
+#Load modules
 include("atmos/atmos.jl")
+include("src/sizing/wsize.jl")
+include("src/mission/mission.jl")
 include("src/aero/aero.jl")
 include("src/structures/structures.jl")
 include("src/propsys/propsys.jl")
+include("src/balance/balance.jl")
+
+# Off-design performance via BADA file like output
+#  and LTO output for EDB points for use in AEIC
+include("src/mission/odperformance.jl")
+include("src/mission/LTO.jl")
+include("src/mission/AircraftDeck.jl")
+
+include("src/fuel/hydrogen.jl")
+include("engine/PT.inc")
+
+# Input and output functions
+include("src/IO/outputs.jl")
+include("src/IO/savemodel.jl")
+
+include("src/cost/cost_est.jl")
+include("src/utils/printBADA.jl")
+
 using .atmosphere
 using .aerodynamics
 using .structures
 using .propsys
+
+#------------------------------------------------------
+#End imports/loading files
+#------------------------------------------------------
+
+# Derived constants
+TSL, pSL, ρSL, aSL, μSL = atmos(0.0)
+RSL =  pSL/(ρSL * TSL)
+ρAir = ρSL
+
+# ----------------------
+# Sizing function
+# ----------------------
+time_writing = 0.0
+time_run_NPSS = 0.0
+
+initwgt = 0
+saveOD = false
+track_fig = nothing
+opt_iter_counter = 0
+
+function size_aircraft(iter, initwgt, Ldebug, printiter, saveOD)
+    global time_writing = 0.0
+    global time_run_NPSS = 0.0
+    parpt[ipt_time_NPSS] = 0.0
+    parpt[ipt_calls_NPSS] = 0
+    global opt_iter_counter += 1
+
+    Ldebug && println("Max weight iterations = $iter")
+    wsize(pari, parg, parm, view(para,:,:,1), view(pare, :,:,1),
+                    iter, 0.5, 0.9, 0.5, initwgt, 1, 1, Ldebug, printiter, saveOD)
+    
+    # global track_fig = stickfig(parg, pari,  parm; ax = track_fig)
+    if (opt_iter_counter%5 == 0) || (opt_iter_counter == 1)
+        global track_fig = plot_details(parg, pari, para, parm; ax = track_fig)
+    end
+end
