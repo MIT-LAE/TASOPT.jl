99f7136da2ddadd38eaab58ade6b2f32ac1d0053
Prashanth <prash@mit.edu>
2023-01-18 10:25:00 -0500

    add NPSS TF funcs and include into propsys module

--- src/propsys/propsys.jl
+++ src/propsys/propsys.jl
@@ -5,11 +5,14 @@ Will cover alternate engine models - NPSS vs Drela's orig. model vs pyCycle
 """
 module propsys
 
-export NPSS_run, startNPSS, endNPSS, NPSS_TEsys, NPSS_TEsysOD
+export NPSS_run, startNPSS, endNPSS, NPSS_TEsys, NPSS_TEsysOD, 
+NPSS_TFsys, NPSS_TFsysOD, NPSS_TFsysOD2
+
 include("../misc/index.inc")
 include("../misc/constants.jl")
 
 include("PMSM.jl")
 include("NPSS_functions.jl")
+include("NPSS_TF_funcs.jl")
 
 end
