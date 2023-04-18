0ec344e5fde1cbc0bfa11504edf18c15c93cf3db
Sicheng He <hschsc@umich.edu>
2023-04-18 13:44:30 -0400

    add tfweight and debug

---
+++ example/example_drela_run.jl
@@ -0,0 +1,26 @@
+
+using Pkg
+Pkg.activate("../")
+Pkg.instantiate()
+
+# 1) Load TASOPT
+include("../tasopt.jl")
+
+# 2) Include input file for desired aircraft/
+#  can also be a saved model
+include("737input_2.jl")
+
+# 3) Size aircraft
+time_wsize = @elapsed size_aircraft(35, 0, false, true, saveOD)
+println("Time to size aircraft = $time_wsize s")
+# 4) Visualize outputs
+# Output resulting geometry of aircraft
+geometry(parg)
+
+# Show weight buildup of the aircraft
+weight_buildup(parg)
+
+# 5) Plot figures
+pygui(true)
+stickfig(parg,pari,parm)
+plt.savefig("../../example/Example.png")
