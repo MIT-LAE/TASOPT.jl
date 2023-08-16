05cab52bc43b08fdd3d2381ffd8e356b6263cf8c
Prashanth Prakash <prash@mit.edu>
2023-08-16 18:10:47 -0400

    Update benchmark.jl

--- test/benchmark.jl
+++ test/benchmark.jl
@@ -5,7 +5,8 @@ println("Current location $(pwd())")
 Pkg.activate("../")
 Pkg.instantiate()
 
-using BenchmarkTools, BenchmarkPlots, StatsPlots
+using Profile
+using BenchmarkTools
 
 println("Loading TASOPT...")
 include("../tasopt.jl")
@@ -14,6 +15,12 @@ Pkg.precompile()
 println("Loading input file...")
 include("input.jl")
 
+println("\nNotes (from BenchmarkTools Manual):
+- The minimum is a robust estimator for the location parameter of the time distribution, and should not be considered an outlier
+- The median, as a robust measure of central tendency, should be relatively unaffected by outliers
+- The mean, as a non-robust measure of central tendency, will usually be positively skewed by outliers
+- The maximum should be considered a primarily noise-driven outlier, and can change drastically between benchmark trials.\n\n")
+
 println("Start Benchmarking...")
 
 function benchmark_fuseBL()
@@ -21,6 +28,33 @@ println("---------------------------------------")
 println("Fuselage boundary layer calculations")
 println("---------------------------------------")
 
+#Inputs from Fortran
+xnose   = 0.000   
+xend    = 37.79    
+xblend1 = 6.096    
+xblend2 = 29.56    
+Sfuse = 13.50     
+anose = 1.649     
+btail = 2.000     
+ifclose = 0 
+Mach = 0.80
+nc = 30 
+nbldim = 60
+xbl = zeros(nbldim)
+zbl = zeros(nbldim)
+sbl = zeros(nbldim)
+dybl = zeros(nbldim)
+uinv = zeros(nbldim)
+
+println("Benchmarking... axisol")
+
+bench = @benchmarkable aerodynamics.axisol!($xnose,$xend,$xblend1,$xblend2,
+$Sfuse, $anose, $btail, $ifclose,
+$Mach, $nc, $nbldim,  $xbl, $zbl, $sbl, $dybl, $uinv) seconds=30 evals=50
+bench_axisol = run(bench)
+
+#results
+nbl =           47 ;iblte =          31 ;
 # Blax benchmarks
 # Load required inputs:
 ndim, n, ite = 60, 47, 31
@@ -79,11 +113,12 @@ b = @benchmarkable fusebl!($pari, $parg, $para, $ipcruise1) seconds=30 evals=5
 bench_fusebl = run(b)
 
 println("Benchmark results...")
-println("\nNotes (from BenchmarkTools Manual):
-- The minimum is a robust estimator for the location parameter of the time distribution, and should not be considered an outlier
-- The median, as a robust measure of central tendency, should be relatively unaffected by outliers
-- The mean, as a non-robust measure of central tendency, will usually be positively skewed by outliers
-- The maximum should be considered a primarily noise-driven outlier, and can change drastically between benchmark trials.")
+
+println("---------------------------------------")
+println("axisol (FORTRAN on MacPro M2 ~ 30 μs)")
+println("---------------------------------------")
+display(bench_axisol)
+
 println("---------------------------------------")
 println("blax (FORTRAN on MacPro M2 ~ 1.9 ms)")
 println("---------------------------------------")
@@ -140,22 +175,56 @@ fLo = -0.29999999999999999
 ktip = 16
 Lspec = true
 CLsurfsp =[1.2502595056643222, 1.1976021933848557E-002] 
+idim::Int = 360
+jdim::Int = 360
+ t     = zeros(Float64, jdim)
+ y     = zeros(Float64, jdim)
+ yp    = zeros(Float64, jdim)
+ z     = zeros(Float64, jdim)
+ zp    = zeros(Float64, jdim)
+ gw    = zeros(Float64, jdim)
+
+ yc    = zeros(Float64, idim)
+ ycp   = zeros(Float64, idim)
+ zc    = zeros(Float64, idim)
+ zcp   = zeros(Float64, idim)
+ gc    = zeros(Float64, idim)
+ vc    = zeros(Float64, idim)
+ wc    = zeros(Float64, idim)
+ vnc   = zeros(Float64, idim)
 
 println("Benchmarking... trefftz1")
-b = @benchmarkable aerodynamics.trefftz1($nsurf, $npout,
+bench = @benchmarkable aerodynamics.trefftz1($nsurf, $npout,
                         $npinn, $npimg, 
 	                    $Sref, $bref,
 	                    $b,$bs,$bo,$bop, $zcent,
 	                    $po,$gammat,$gammas, $fLo, $ktip,
-                       $Lspec,$CLsurfsp) seconds=30 evals=50
-bench_trefftz = run(b)
+                       $Lspec,$CLsurfsp,
+                        $t, $y, $yp, $z, $zp, $gw, $yc, $ycp, $zc, $zcp, $gc, $vc, $wc, $vnc) seconds=30 evals=100
+bench_trefftz = run(bench)
+
+wa = aerodynamics.tfp_workarrays(t, y, yp, z, zp, gw, yc, ycp, zc, zcp, gc, vc, wc, vnc)
+tf = aerodynamics.tfp(nsurf, npout, npinn, npimg, 
+Sref, bref, b, bs, bo, bop, zcent, po, gammat, gammas, CLsurfsp, ktip, wa)
+bench = @benchmarkable aerodynamics.trefftz($tf) seconds=30 evals=100
+bench_trefftz_struct = run(bench)
+
+# You can use the following tempalte to profile code in the REPL
+# Profile.init(delay = 1e-6)
+# a = @profile (for i=1:10000; aerodynamics.trefftz1(nsurf, npout,
+# npinn, npimg, 
+# Sref, bref,
+# b,bs,bo,bop, zcent,
+# po,gammat,gammas, fLo, ktip,
+# Lspec,CLsurfsp); end)
+# # Profile.print()
+
 
 println("Benchmarking... cdsum!")
-b = @benchmarkable aerodynamics.cdsum!($pari, $parg, 
+bench = @benchmarkable aerodynamics.cdsum!($pari, $parg, 
 $view(para, :, 10),
 $view(pare,:, 10), $(1)) seconds=30 evals=1
-bench_cdsum = run(b)
-
+bench_cdsum = run(bench)
 
 
 println("---------------------------------------")
@@ -167,7 +236,10 @@ println("---------------------------------------")
 println("trefftz (FORTRAN on MacPro M2 ~ 4.6 μs)")
 println("---------------------------------------")
 display(bench_trefftz)
-
+println("---------------------------------------")
+println("trefftz struct (FORTRAN on MacPro M2 ~ 4.6 μs)")
+println("---------------------------------------")
+display(bench_trefftz_struct)
 println("---------------------------------------")
 println("cdsum (FORTRAN on MacPro M2 ~ 5.8 - 6.0 μs)")
 println("---------------------------------------")
@@ -175,6 +247,67 @@ display(bench_cdsum)
 
 end
 
+
+function benchmark_gasfuns()
+t = [   175.00e0, 200.00e0, 225.00e0, 250.00e0, 275.00e0, 300.00e0,
+        325.00e0, 350.00e0, 375.00e0, 400.00e0, 450.00e0, 500.00e0,
+        550.00e0, 600.00e0, 650.00e0, 700.00e0, 750.00e0, 800.00e0,
+        850.00e0, 900.00e0, 950.00e0, 1000.00e0, 1050.00e0, 1100.00e0,
+        1150.00e0, 1200.00e0, 1250.00e0, 1300.00e0, 1350.00e0, 1400.00e0,
+        1500.00e0, 1600.00e0, 1700.00e0, 1800.00e0, 1900.00e0, 2000.00e0,
+        2100.00e0, 2200.00e0, 2300.00e0, 2400.00e0, 2500.00e0, 2600.00e0,
+        2700.00e0, 2800.00e0, 2900.00e0, 3000.00e0, 3500.00e0, 4000.00e0,
+        4500.00e0, 5000.00e0, 5500.00e0, 6000.00e0]
+    tl = [ 5.16479e0, 5.29832e0, 5.41610e0, 5.52146e0, 5.61677e0, 5.70378e0,
+        5.78383e0, 5.85793e0, 5.92693e0, 5.99146e0, 6.10925e0, 6.21461e0,
+        6.30992e0, 6.39693e0, 6.47697e0, 6.55108e0, 6.62007e0, 6.68461e0,
+        6.74524e0, 6.80239e0, 6.85646e0, 6.90776e0, 6.95655e0, 7.00307e0,
+        7.04752e0, 7.09008e0, 7.13090e0, 7.17012e0, 7.20786e0, 7.24423e0,
+        7.31322e0, 7.37776e0, 7.43838e0, 7.49554e0, 7.54961e0, 7.60090e0,
+        7.64969e0, 7.69621e0, 7.74066e0, 7.78322e0, 7.82405e0, 7.86327e0,
+        7.90101e0, 7.93737e0, 7.97247e0, 8.00637e0, 8.16052e0, 8.29405e0,
+        8.41183e0, 8.51719e0, 8.61250e0, 8.69951e0]
+    cp = [ 1039.00e0, 1039.00e0, 1039.00e0, 1039.10e0, 1039.30e0, 1039.60e0,
+        1040.20e0, 1041.00e0, 1042.20e0, 1043.80e0, 1049.00e0, 1056.00e0,
+        1065.00e0, 1075.00e0, 1086.00e0, 1098.00e0, 1110.00e0, 1122.00e0,
+        1134.00e0, 1146.00e0, 1157.00e0, 1167.00e0, 1177.00e0, 1187.00e0,
+        1196.00e0, 1204.00e0, 1212.00e0, 1219.00e0, 1226.00e0, 1232.00e0,
+        1244.00e0, 1254.00e0, 1263.00e0, 1271.00e0, 1278.00e0, 1284.00e0,
+        1290.00e0, 1295.00e0, 1300.00e0, 1304.00e0, 1307.00e0, 1311.00e0,
+        1314.00e0, 1317.00e0, 1320.00e0, 1323.00e0, 1333.00e0, 1342.00e0,
+        1349.00e0, 1355.00e0, 1362.00e0, 1369.00e0]
+    cpt = [ 0.00051e0, -0.00051e0, 0.00154e0, 0.00638e0, 0.00894e0, 0.01784e0,
+        0.02770e0, 0.03935e0, 0.05488e0, 0.07713e0, 0.12347e0, 0.16099e0,
+        0.19255e0, 0.20879e0, 0.23230e0, 0.24203e0, 0.23959e0, 0.23962e0,
+        0.24193e0, 0.23265e0, 0.20745e0, 0.19753e0, 0.20243e0, 0.19275e0,
+        0.16659e0, 0.16090e0, 0.14980e0, 0.13990e0, 0.13058e0, 0.11776e0,
+        0.11229e0, 0.09307e0, 0.08542e0, 0.07524e0, 0.06361e0, 0.06033e0,
+        0.05506e0, 0.04944e0, 0.04717e0, 0.03186e0, 0.03539e0, 0.03659e0,
+        0.02826e0, 0.03036e0, 0.03030e0, 0.02844e0, 0.01722e0, 0.01667e0,
+        0.01211e0, 0.01288e0, 0.01437e0, 0.01363e0]
+    h = [ -0.12781e6, -0.10184e6, -0.07586e6, -0.04989e6, -0.02391e6, 0.00208e6,
+        0.02808e6, 0.05409e6, 0.08013e6, 0.10620e6, 0.15851e6, 0.21113e6,
+        0.26415e6, 0.31765e6, 0.37167e6, 0.42626e6, 0.48146e6, 0.53726e6,
+        0.59366e6, 0.65067e6, 0.70825e6, 0.76635e6, 0.82495e6, 0.88405e6,
+        0.94363e6, 1.00363e6, 1.06403e6, 1.12481e6, 1.18594e6, 1.24739e6,
+        1.37119e6, 1.49611e6, 1.62197e6, 1.74868e6, 1.87613e6, 2.00424e6,
+        2.13294e6, 2.26220e6, 2.39195e6, 2.52216e6, 2.65271e6, 2.78361e6,
+        2.91486e6, 3.04641e6, 3.17826e6, 3.31041e6, 3.97465e6, 4.64341e6,
+        5.31625e6, 5.99224e6, 6.67146e6, 7.35422e6]
+    s = [ -0.55312e3, -0.41438e3, -0.29201e3, -0.18253e3, -0.08349e3, 0.00695e3,
+        0.09019e3, 0.16730e3, 0.23916e3, 0.30647e3, 0.42969e3, 0.54056e3,
+        0.64162e3, 0.73471e3, 0.82118e3, 0.90210e3, 0.97826e3, 1.05028e3,
+        1.11866e3, 1.18382e3, 1.24608e3, 1.30569e3, 1.36287e3, 1.41785e3,
+        1.47082e3, 1.52189e3, 1.57121e3, 1.61888e3, 1.66502e3, 1.70971e3,
+        1.79513e3, 1.87574e3, 1.95204e3, 2.02446e3, 2.09337e3, 2.15908e3,
+        2.22187e3, 2.28200e3, 2.33968e3, 2.39510e3, 2.44839e3, 2.49973e3,
+        2.54926e3, 2.59710e3, 2.64337e3, 2.68817e3, 2.89293e3, 3.07152e3,
+        3.23001e3, 3.37245e3, 3.50192e3, 3.62073e3]
+
+        bench = @benchmarkable gas_N2($t1, $t, $tl, $cp, $cpt, $h, $s) seconds=30 evals=50
+end
+
 include("../Models/ZISA/ZIA_SAF_BLI_10_8_0.647_17.4.mdl")
-benchmark_fuseBL()
+
+# benchmark_fuseBL()
 benchmark_drag()
