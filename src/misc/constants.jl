7b4cd8d3b3df793e64073dbb3dd05644eb33dce3
Prashanth Prakash <prash@mit.edu>
2023-08-25 14:49:23 -0400

    export constants

--- src/misc/constants.jl
+++ src/misc/constants.jl
@@ -1,3 +1,6 @@
+export ft_to_m, in_to_m, nmi_to_m, deg_to_rad, lbf_to_N, kts_to_mps, hp_to_W, lb_N
+export gee, gamSL, cpSL, μAir, pref, Tref
+
 const ft_to_m = 0.3048
 const in_to_m = 0.0254
 const nmi_to_m = 1852.0
@@ -5,10 +8,11 @@ const deg_to_rad = π / 180.0
 const lbf_to_N = 4.448222
 const kts_to_mps = 0.51444
 const hp_to_W = 745.7
+const lb_N = 0.22480902473348890
+
 const gee = 9.81
 const gamSL = 1.4
 const cpSL = 1004.0
 const μAir = 1.78e-5
 const pref = 101320.00000000000
 const Tref = 288.19999999999999
-const lb_N = 0.22480902473348890
