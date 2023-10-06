16d8db7ef84d05358f00952a27552ef6c72a3c02
Prashanth Prakash <prash@mit.edu>
2023-10-06 13:16:43 -0400

    fix drela doc links

--- docs/make.jl
+++ docs/make.jl
@@ -4,38 +4,38 @@ using TASOPT
 
 push!(LOAD_PATH, "../src")
 
+# Shorthands for convenience
 aerodynamics = TASOPT.aerodynamics
 structures = TASOPT.structures
 engine = TASOPT.engine
 aircraft = TASOPT.aircraft
 
 using Documenter
-
 makedocs(
     remotes = nothing,
     sitename = "TASOPT.jl Documentation",
-    pages = [ "Home" => "index.md", 
-    "Atmospheric properties" => "atmos/atmos.md",
-    "Aerodynamics" => Any[
-        "aero/geometry.md",
-        "aero/lift.md",
-        "aero/drag.md",
-        "aero/moment.md",
+    pages=["Home" => "index.md",
+        "Atmospheric properties" => "atmos/atmos.md",
+        "Aerodynamics" => Any[
+            "aero/geometry.md",
+            "aero/lift.md",
+            "aero/drag.md",
+            "aero/moment.md",
+        ],
+        "Structures" => Any["structures/wing.md",
+            "structures/fuselage.md",
+            "structures/fueltanks.md"
+        ],
+        "Propulsion systems" => Any[
+            "propulsion/propsys.md",
+        ],
+        "Stability" => "balance/balance.md",
+        "Sizing" => "sizing/sizing.md",
+        "Miscellaneous" => Any[
+            "misc/aircraft.md",
+            "misc/misc.md",
+            "misc/dreladocs.md"
+        ]
     ],
-    "Structures" => Any[
-       
-        "structures/wing.md",
-        "structures/fuselage.md",
-        "structures/fueltanks.md"
-    ],
-    "Propulsion systems" => Any[
-        "propulsion/propsys.md",
-    ],
-    "Stability" => "balance/balance.md",
-    "Sizing" => "sizing/sizing.md",
-    "Miscellaneous" => Any[
-        "misc/aircraft.md",
-        "misc/misc.md",
-        # "misc/dreladocs.md"
-    ]
-    ],)
+    )
+
