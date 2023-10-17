da4dfb58b679f6220d2215b1c53cc7b8e80a9c68
Prashanth Prakash <prash@mit.edu>
2023-10-17 11:16:53 -0400

    remove NPSS stuff from propsys.jl

--- src/propsys/propsys.jl
+++ src/propsys/propsys.jl
@@ -5,14 +5,12 @@ Will cover alternate engine models - NPSS vs Drela's orig. model vs pyCycle
 """
 module propsys
 
-export NPSS_run, startNPSS, endNPSS, NPSS_TEsys, NPSS_TEsysOD, 
-NPSS_TFsys, NPSS_TFsysOD, NPSS_TFsysOD2
+export NPSS_run, startNPSS, endNPSS
 
 include("../misc/index.inc")
 include("../misc/constants.jl")
 
 include("PMSM.jl")
 include("NPSS_functions.jl")
-include("NPSS_TF_funcs.jl")
 
 end
