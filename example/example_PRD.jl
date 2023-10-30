78c72a7251d247c36ac2c8ab3e40e3ad3c9a1cdd
aditeya <aditeya@mit.edu>
2023-10-30 14:46:35 -0400

    PRD scope fix + Optimization improvements (#101)
    * Payload Range tutorial
    
    * Start cleaning up woper.jl
    
    Still need to remove hard coded bits. Probably best if I do this after cloning it. Going to try that.
    
    * default toml file for PRD
    
    * pushing relevant files for PRD tutorial
    
    * added optimization tutorial
    
    * Update optimization.md
    
    Removed some remnants of hydrogen tanks and some minor formatting
    
    * moving tutorial input files to example + minor changes
    
    * PRD scope fix, opt Tt4+iept3 addition
    
    * added docs figures + opt improvements
    
    * cleaning up prd file, plot fix
    
    * better optimziation script with OPR and FPR
    
    Co-authored-by: Aditeya Shukla <aditeyashukla@gmail.com>
    Co-authored-by: Prashanth <prash@mit.edu>

--- example/example_PRD.jl
+++ example/example_PRD.jl
@@ -43,107 +43,98 @@ end
     # End loop if difference between min range and max range below threshold
     # OR payload <= 0
 
+function PayloadRange()
+    lbf_to_N = 4.448222
+    MAX_PAYLOAD = pax2N(230)
 
-lbf_to_N = 4.448222
-MAX_PAYLOAD = pax2N(230)
+    Range_arr_nmi = LinRange(100, 1000, 4)
+    Range_arr = Range_arr_nmi .* 1852.0
+    Max_range = Range_arr[4]
 
-R_thresh = Inf
+    curr_range = Range_arr[1]
+    prev_range = 0.1
 
-Range_arr_nmi = LinRange(100, 1000, 4)
-Range_arr = Range_arr_nmi .* 1852.0
-Max_range = Range_arr[4]
+    # 5. Initialze some variables
+    NPSS = Base.Process
+    NPSS_PT = true
+    itermax = 15
+    initeng = 0
 
-curr_range = Range_arr[1]
-prev_range = 0.1
+    x_range = []
+    y_payload = []
+    y_WTO = []
+    y_Wfuel = []
+    y_Wemtpy = []
 
-# 5. Initialze some variables
-NPSS = Base.Process
-NPSS_PT = true
-itermax = 15
-initeng = 0
-Ldebug = false
-saveOD = false
+    SEC_B = false
 
-x_range = []
-y_payload = []
-y_WTO = []
-y_Wfuel = []
-y_Wemtpy = []
+    # Load default model
+    ac = TASOPT.read_aircraft_model(joinpath(TASOPT.__TASOPTroot__, "../example/PRD_input.toml"))
 
+    size_aircraft!(ac)
 
-range_thresh = nmi2km(5) # 50 nmi
-
-SEC_B = false
-
-# Load default model
-ac = TASOPT.read_aircraft_model(joinpath(TASOPT.__TASOPTroot__, "../example/PRD_input.toml"))
-
-time_wsize = @elapsed size_aircraft!(ac)
-
-
-while MAX_PAYLOAD >= 0
-    println("-----------------------")
-    println("Starting new set of missions, \n Ranges: ", km2nmi(Range_arr), "\n Payload: ", N2pax(MAX_PAYLOAD))
-    
-    try
-        ac.parm[imRange, 2:5] .= Range_arr
-        ac.parm[imWpay, 2:5] .= MAX_PAYLOAD
+    while MAX_PAYLOAD >= 0
+        println("-----------------------")
+        println("Starting new set of missions, \n Ranges: ", km2nmi(Range_arr), "\n Payload: ", N2pax(MAX_PAYLOAD))
         
-        for mi in 2:size(ac.parm)[2]
-            prev_range = curr_range
-            curr_range = ac.parm[imRange, mi]
-            println("Analysing Mission Number: ",mi)
-            println("Range: ",curr_range)
-            @views TASOPT.woper(ac.pari,ac.parg,ac.parm[:,mi:mi],ac.para[:,:,mi:mi],ac.pare[:,:,mi:mi], ac.para[:,:,1:1],ac.pare[:,:,1:1], itermax,initeng, NPSS_PT, NPSS)
-            if (ac.parm[imWfuel,mi] - ac.parg[igWfuel] > 1) || (ac.parm[imWTO,mi] - ac.parg[igWMTO] > 1 )
-                printstyled("Mission designed beyond capacity!", "\n"; color=:red)
-                println([ac.parg[igWfuel], ac.parm[imWfuel,mi], ac.parg[igWMTO], ac.parm[imWTO,mi]])
-                throw(UndefVarError([ac.parg[igWfuel], ac.parm[imWfuel,mi], ac.parg[igWMTO], ac.parm[imWTO,mi]]))
-            end
-            #---- max TO weight
-            WMTO = ac.parg[igWMTO]
-            # ---- zero-fuel weight for this mission
-            Wzero = WMTO-ac.parg[igWfuel]-ac.parg[igWpay]
+        try
+            ac.parm[imRange, 2:5] .= Range_arr
+            ac.parm[imWpay, 2:5] .= MAX_PAYLOAD
             
-            append!(y_Wemtpy, Wzero)
-            append!(x_range, curr_range)
-            append!(y_payload, MAX_PAYLOAD)
-            append!(y_Wfuel,  ac.parm[imWfuel,mi])
-            append!(y_WTO,  ac.parm[imWTO,mi])
-            if SEC_B
-                println("decreasing payload since Section B")
-                MAX_PAYLOAD = max(0, MAX_PAYLOAD-pax2N(1))
+            for mi in 2:size(ac.parm)[2]
+                prev_range = curr_range
+                curr_range = ac.parm[imRange, mi]
+                println("Analysing Mission Number: ",mi)
+                println("Range: ",curr_range)
+                @views TASOPT.woper(ac.pari,ac.parg,ac.parm[:,mi:mi],ac.para[:,:,mi:mi],ac.pare[:,:,mi:mi], ac.para[:,:,1:1],ac.pare[:,:,1:1], itermax,initeng, NPSS_PT, NPSS)
+                if (ac.parm[imWfuel,mi] - ac.parg[igWfuel] > 1) || (ac.parm[imWTO,mi] - ac.parg[igWMTO] > 1 )
+                    printstyled("Mission designed beyond capacity!", "\n"; color=:red)
+                    println([ac.parg[igWfuel], ac.parm[imWfuel,mi], ac.parg[igWMTO], ac.parm[imWTO,mi]])
+                    throw(UndefVarError([ac.parg[igWfuel], ac.parm[imWfuel,mi], ac.parg[igWMTO], ac.parm[imWTO,mi]]))
+                end
+                #---- max TO weight
+                WMTO = ac.parg[igWMTO]
+                # ---- zero-fuel weight for this mission
+                Wzero = WMTO-ac.parg[igWfuel]-ac.parg[igWpay]
+                
+                append!(y_Wemtpy, Wzero)
+                append!(x_range, curr_range)
+                append!(y_payload, MAX_PAYLOAD)
+                append!(y_Wfuel,  ac.parm[imWfuel,mi])
+                append!(y_WTO,  ac.parm[imWTO,mi])
+                if SEC_B
+                    println("decreasing payload since Section B")
+                    MAX_PAYLOAD = max(0, MAX_PAYLOAD-pax2N(1))
+                end
             end
+            println("PRD converged for all: Increasing Ranges")
+            prev_range = curr_range
+            Max_range = prev_range+(500* 1852.0)
+        catch
+            println("Decreasing Payload and range")
+            prev_range = curr_range
+            Max_range = prev_range+ ((Max_range-prev_range)*0.75)
+            MAX_PAYLOAD = MAX_PAYLOAD-pax2N(5)
+            SEC_B = true
         end
-        println("PRD converged for all: Increasing Ranges")
-        prev_range = curr_range
-        Max_range = prev_range+(500* 1852.0)
-    catch
-        println("PRD failed: Decreasing Payload and range")
-        prev_range = curr_range
-        Max_range = prev_range+ ((Max_range-prev_range)*0.75)
-        MAX_PAYLOAD = MAX_PAYLOAD-pax2N(5)
-        SEC_B = true
+        Range_arr = LinRange(prev_range, Max_range, 4)
+        println("-----------------------", abs(Max_range-prev_range))
     end
-    Range_arr = LinRange(prev_range, Max_range, 4)
-    println("-----------------------", abs(Max_range-prev_range))
+
+
+
+    figure()
+    y_OEW = y_Wemtpy .+ y_payload
+    plot(x_range ./ (1000*1852.0), y_OEW./ (9.8*1000), linestyle="-",  color="b", label="OEW + Payload ")
+
+    xlabel("Range (1000 nmi)")
+    ylabel("Weight (1000 kg)")
+    title("Payload Range Plot")
+    legend()
+    grid()
+
+    savefig("./example/PayloadRangeExample.png")
+
 end
 
-
-using PyPlot
-figure()
-y_OEW = y_Wemtpy .+ y_payload
-plot(x_range ./ (1000*1852.0), y_OEW./ (4.448222* 1000), linestyle="-",  color="b", label="OEW + Payload ")
-# plot(x_range./ (1000), (y_WTO.-y_Wfuel)./ (4.448222* 1000), marker="o", linestyle="-", color="g", label="MTO-wf")
-# plot(x_range./ (1000), (y_WTO)./ (4.448222* 1000), marker="o", linestyle="-", color="g", label="MTO")
-# plot(x_range./ (1000), y_Wfuel./ (4.448222* 1000), marker="o", linestyle="-", color="k", label="Wfuel")
-# plot(x_range./ (1000), y_payload./ (4.448222* 1000), linestyle="-", color="b", label="Payload weight")
-
-
-xlabel("Range (1000 nmi)")
-ylabel("Weight (1000 lbs)")
-title("Payload Range Plot")
-legend()
-grid()
-
-savefig("./example/PayloadRangeExample_last.png")
+PayloadRange()
