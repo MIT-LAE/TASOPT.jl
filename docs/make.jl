517adf97e913dafd7e59baf944efbdaaa1f64b6e
Prashanth Prakash <prash@mit.edu>
2023-12-04 13:51:18 -0500

    gas calc markdown is missing

--- docs/make.jl
+++ docs/make.jl
@@ -36,7 +36,7 @@ makedocs(
     "Propulsion systems" => Any[
             "propulsion/propsys.md",
             "propulsion/hxfun.md",
-            "propulsion/gascalc.md"
+            # "propulsion/gascalc.md"
         ],
     "Stability" => "balance/balance.md",
     "Mission and sizing" => Any[
