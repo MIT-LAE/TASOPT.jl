8f728bad30c17204fe244627229d834ce5386dc0
Nicolas Gomez <ngomezve@mit.edu>
2024-02-05 11:36:25 -0500

    improved tank location options

--- src/misc/aircraft.jl
+++ src/misc/aircraft.jl
@@ -21,6 +21,7 @@ For devs: the indices for accessing specific data are defined in `/src/misc/inde
 """
 
 mutable struct fuselage_tank
+    placement::String
     t_insul::Array{Float64}
     k_insul::Array{Float64}
     rho_insul::Array{Float64}
