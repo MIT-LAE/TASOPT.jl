dc951b34c7a242e9f50ee887d401a65ea48b3cd3
Prashanth Prakash <prash@mit.edu>
2024-02-06 13:56:20 -0500

    Add hx tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,5 +4,6 @@ using Test
 @testset verbose=true "TASOPT" begin
     include("unit_test_structures.jl")
     include("unit_test_aero.jl")
+    include("unit_test_heat_exchanger.jl")
     include("regression_test_wsize.jl")
 end
