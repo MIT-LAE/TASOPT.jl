0e3bedf75d4c0727c4ba1eef32beba70deb1f40b
Nicolas Gomez <ngomezve@mit.edu>
2024-03-12 13:39:25 -0400

    added support for double decker

--- src/IO/read_input.jl
+++ src/IO/read_input.jl
@@ -299,6 +299,7 @@ readgeom(x) = read_input(x, geom, dgeom)
     #Boolean to check if cabin length has to be recalculated; if true, this is done 
     #after loading the wing and stabilizer positions
     calculate_cabin = readgeom("calculate_cabin_length") 
+    pari[iidoubledeck] = readgeom("double_decker") 
 
     parg[igseatpitch] = Len(readgeom("seat_pitch"))
     parg[igseatwidth] = Len(readgeom("seat_width"))
