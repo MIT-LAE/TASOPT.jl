3002987388e20379141822d9f7106b1f5e01a1d4
Jonas J. Gonzalez <argonaut@mit.edu>
2024-03-15 00:25:44 -0400

    io test and doc skeleton

---
+++ test/unit_test_io.jl
@@ -0,0 +1,26 @@
+#tests io functionalities
+
+@testset "io" begin
+    
+#testing: readable TOML saves
+
+    #run baseline aircraft,
+
+    
+    
+
+#testing: quicksaves and loads
+
+
+
+#testing: output to .csv
+
+
+
+
+
+end
+
+
+
+
