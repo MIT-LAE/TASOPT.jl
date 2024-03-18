53d13353f54ea1b2dbb0537e6ee769407924a729
Prashanth Prakash <prash@mit.edu>
2024-03-18 00:28:34 -0400

    add materials unit tests and add to runtests.jl

---
+++ test/unit_test_materials.jl
@@ -0,0 +1,34 @@
+@testset "Materials" verbose=true begin
+    database = TASOPT.MaterialProperties
+    @testset "Structural Alloys" begin
+        Al7075 = StructuralAlloy("Al-7075")
+        @test Al7075.ρ == database["Al-7075"]["density"]
+        @test Al7075.E == database["Al-7075"]["youngs_modulus"]
+        #Check that safety_factors are being applied
+        @test Al7075.σmax == database["Al-7075"]["YTS"]/1.1/1.5 
+        Al = StructuralAlloy("Al-7075"; max_avg_stress = 1.0, safety_factor = 2.0)
+        @test Al.σmax == database["Al-7075"]["YTS"]/1.0/2.0
+        @test Al.τmax == database["Al-7075"]["shear_strength"]/1.0/2.0
+    end
+
+    @testset "Conductors" begin
+        Cu = Conductor("Cu")
+        @test Cu.ρ == database["Cu"]["density"] 
+        @test TASOPT.resxden(Cu) == 0.000150528
+        @test TASOPT.resistivity(Cu) == database["Cu"]["resistivity"]
+        @test TASOPT.resistivity(Cu, 300.0) ≈ 1.7264923200000005e-8
+    end
+
+    @testset "Insulators" begin
+        PTFE = Insulator("PTFE")
+        @test PTFE.ρ == database["PTFE"]["density"]
+    end
+    
+    # Test that unreasonable requests throw errors
+    ## Try making something structural with gold
+    @testset "Material errors" begin
+        @test_throws ErrorException StructuralAlloy("Ag")
+        @test_throws ErrorException Conductor("PTFE")
+        @test_throws ErrorException Insulator("Cu")
+    end
+end
