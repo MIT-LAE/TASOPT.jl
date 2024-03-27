b790e58da7efe2f388c113413173dabb785b4137
Nicolas Gomez <ngomezve@mit.edu>
2024-03-27 11:33:30 -0400

    better saturation fuel properties

--- src/fuel/fuel_properties.jl
+++ src/fuel/fuel_properties.jl
@@ -1,7 +1,7 @@
 """
     cryo_fuel_properties(fuel::String, p::Float64)
 
-Calculates the temperature and density of a cryogenic fuel inside a tank, using fits to data from NIST.
+Calculates the saturation temperature and density of a cryogenic fuel inside a tank, using fits to data from NIST.
 !!! details "🔃 Inputs and Outputs"
     **Inputs:**
     - `fuel::String`: name of the fuel. 
@@ -13,13 +13,13 @@ Calculates the temperature and density of a cryogenic fuel inside a tank, using 
 function cryo_fuel_properties(fuel::String, p::Float64)
     x = p / 101325 #pressure in atm
 
-    #Fits to NIST data at p = [0.5,1,1.5,2,2.5,3,4,5,10] atm
+    #Fits to NIST data at p increments of 0.1 atm from 0.1 to 10 atm
     if fuel == "LH2" 
-        Tfuel = 20.3839 * x^0.182022 
-        ρfuel = -2.88304E-02*x^3 + 4.93319E-01*x^2 - 4.61334E+00*x + 7.51570E+01
+        Tfuel = 3.35772E-04*x^6 + 1.14637E-02*x^5 - 1.54871E-01*x^4 + 1.05803E+00*x^3 - 3.93770E+00*x^2 + 9.09205E+00*x + 1.42913E+01
+        ρfuel = 2.58354E-04*x^6 - 8.90930E-03*x^5 + 1.21248E-01*x^4 - 8.37637E-01*x^3 + 3.14780E+00*x^2 - 8.42843E+00*x + 7.68629E+01
     elseif fuel == "CH4"
-        Tfuel = 111.69030 * x^0.12093
-        ρfuel = -1.49198E-01*x^3 + 2.85956E+00*x^2 - 2.20464E+01*x + 4.42724E+02
+        Tfuel = -9.38765E-04*x^6 + 3.25637E-02*x^5 - 4.49376E-01*x^4 + 3.16285E+00*x^3 - 1.22945E+01*x^2 + 3.00925E+01*x + 9.09645E+01
+        ρfuel = 1.23474E-03*x^6 - 4.28482E-02*x^5 + 5.91776E-01*x^4 - 4.17205E+00*x^3 + 1.62724E+01*x^2 - 4.14826E+01*x + 4.51398E+02
     end
     return Tfuel, ρfuel
 end
