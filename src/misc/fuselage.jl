cb1fcea3b58a1399906d7b96f3318ffa888b81fd
Aditeya Shukla <aditeyashukla@gmail.com>
2024-04-09 11:50:34 -0400

    changed fusew() to fusew!()

--- src/misc/fuselage.jl
+++ src/misc/fuselage.jl
@@ -9,9 +9,10 @@
     layout::FuselageLayout = FuselageLayout()
 
     # Structures
-    material::StructuralAlloy = StructuralAlloy("Al-6061")
+    material::StructuralAlloy = StructuralAlloy("TASOPT-Al")
+    #to do: Add material-> Structural members
     skin::StructuralMember = StructuralMember(σ=1.0342136834273669e8, ρ=2700)
-    shell::StructuralMember = StructuralMember(σ=2.0684273668547338e8, ρ=2700)
+    shell::StructuralMember = StructuralMember(σ=2.0684273668547338e8, ρ=2700) # IS just Skin + Additional
     cone::StructuralMember = StructuralMember()
     floor::StructuralMember = StructuralMember(thickness=0.127)
     insulation::StructuralMember = StructuralMember()
@@ -38,18 +39,4 @@ Eskin = fuselage.material.E #parg[igEcap]
 Ebend = Eskin * fuselage.rEshell
 Gskin = Eskin * 0.5 / (1.0 + 0.3)
 
-TASOPT.fusew(fuselage,13344.666000000001, 219964.5779, 76987.602265, 21996.45779, 7698.7602265000005, 0.0, 0.0, 0.0, 0.0, 0.0, "", 54911.323281976234, 435.0, 22.0, 60.0, 8607.309570000001, 8607.309570000001, 0.4, 0.7, 439929.1558, 439929.15580000007, 5.1591026057637395, 0.3, 1.0, 34.8996, 33.528, 20.165065369407014, 17.3736, 0.0, 2.1336, 36.576, 15.8496, 0.0,Eskin,Ebend,Gskin)
-
-# fusew(Wfix, Wpaymax, Wpadd, Wseat, Wapu, Wengtail, 
-            
-#             Waftfuel,  Wftank_single, ltank, xftank_fuse, tank_placement,
-#              Δp,
-#             Wpwindow, Wppinsul, Wppfloor, 
-#             Whtail, Wvtail, rMh, rMv, Lhmax, Lvmax,
-#             bv, λv, nvtail,
-            
-            
-#             xhtail, xvtail,
-#             xwing, xwbox, cbox,
-#             xfix, xapu, xeng, xfuel,
-#             )
+fusew!(fuselage,13344.666000000001, 219964.5779, 76987.602265, 21996.45779, 7698.7602265000005, 0.0, 0.0, 0.0, 0.0, 0.0, "", 54911.323281976234, 435.0, 22.0, 60.0, 8607.309570000001, 8607.309570000001, 0.4, 0.7, 439929.1558, 439929.15580000007, 5.1591026057637395, 0.3, 1.0, 34.8996, 33.528, 20.165065369407014, 17.3736, 0.0, 2.1336, 36.576, 15.8496, 0.0,Eskin,Ebend,Gskin)
