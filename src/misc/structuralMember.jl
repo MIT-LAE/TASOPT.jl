135c4a8e314c68bb2e5279ad420beea75b44867e
Aditeya Shukla <aditeyashukla@gmail.com>
2024-04-09 12:09:45 -0400

    pasing material to structuralmember

--- src/misc/structuralMember.jl
+++ src/misc/structuralMember.jl
@@ -1,11 +1,12 @@
 @kwdef mutable struct StructuralMember
+    material::StructuralAlloy = StructuralAlloy("TASOPT-Al")
     weight::Float64 = 0 #OR Union{Float64, Nothing}
-    σ::Float64 = 0
+    σ::Float64 = material.σmax
     EIh::Float64 = 0
     EIv::Float64 = 0
     GJ::Float64 = 0
     thickness::Float64 = 0
-    ρ::Float64 = 0
+    ρ::Float64 = material.ρ
     x::Float64 = 0
     # xv::Float64 = 0
     #Material = StructuralAlloy
