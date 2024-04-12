84a0017b99290baaca87bbd14f5ce84e3d8fb095
Sarah Reider <reider@hex.mit.edu>
2024-04-12 08:31:18 -0400

    LTO.jl fixes

--- src/mission/LTO.jl
+++ src/mission/LTO.jl
@@ -1,56 +1,10 @@
-# function LTO(name, NPSS::Base.Process, ifirst, pare; fileout = stdout)
-# nTshaft = parpt[ipt_nTshaft]
-# #LTO values
-# LTOpoints = [1.0, 0.85, 0.3, 0.07]
-# LTOEINOx = zero(4)
-# LTOmdotf = zero(4)
-# LHV = parg[igLHVfuel]
-# # Get Foo based on Tt4 or T/O thrust
-# Tt4 = pare[ieTt4, ipstatic]
-# Foo = pare[ieFe , ipstatic]
-# ip = iptest
-# EIs = zeros(4)
-# mfs = zeros(4)
-# NPSS_success, Foo, η, P, Hrej, heatexcess, 
-# mdotf, deNOx, EINOx1, EINOx2, FAR, Tt3, OPR,
-# Wc3, Tt41, EGT = NPSS_TEsysOD(NPSS, 0.0, 0.0,
-# Foo, 0.0, 0.0, 0.0, 0.0, 0.0, ifirst, parg, parpt, pare, ip)
-# println(fileout, "Aircraft LTO characteristics \t\t"*Dates.format(now(), DateFormat("u dd yyyy")))
-# println(fileout, name)
-# println(fileout, @sprintf("Foo = %.3f [kN]", Foo/1000))
-# println(fileout, @sprintf("%2s) %4s %15s %15s %15s %15s %15s %15s %8s %16s", 
-# "#", "TS", "Fn[kN]", "̇mf_total[kg/s]", "̇mf[kg/s]", "EI(NOₓ)[g/kg]", "Total_NOₓ[g/s]", "NOₓ[g/s]", "deNOₓ[%]", "JetAeqEI(NOₓ)" ))
-
-#     for (i,TS) in enumerate(LTOpoints)
-#         println(@sprintf("Running LTO point %.1f %%", TS*100))
-#         NPSS_success, Ftotal, η, P, Hrej, heatexcess, 
-#         mdotf, deNOx, EINOx1, EINOx2, FAR, Tt3, OPR, Wc3, Tt41, EGT = NPSS_TEsysOD(NPSS, 0.0, 0.0, Foo*TS, 0.0, 0.0, 0.0, 0.0, 0.0, ifirst, parg, parpt, pare, ip)
-#         ifirst  = false
-
-#         println(fileout, 
-#         @sprintf("%2d) %4.2f %15.5f %15.5f %15.5f %15.5f %15.5f %15.5f %8.2f %16.5f",
-#         i, TS, Ftotal/1000, mdotf, mdotf/nTshaft, EINOx2, EINOx2*mdotf, EINOx2*mdotf/nTshaft, deNOx*100, EINOx2*43/LHV))
-#         EIs[i] = EINOx2
-#         mfs[i] = mdotf/nTshaft
-#     end
-#     # for TS in LTOpoints[end:-1:1]
-#     #     NPSS_success, Ftotal, η, P, Hrej, heatexcess, 
-#     #     mdotf, deNOx, EINOx1, EINOx2, FAR, Tt3, OPR, Wc3, Tt41, EGT = NPSS_TEsysOD(NPSS, 0.0, 0.0, Foo*TS, 0.0, 0.0, 0.0, 0.0, 0.0, ifirst, parg, parpt, pare, ip)
-#     # end
-#     println(fileout, "------------------------- AEIC ENG_EI output --------------------------")
-#     for (i,j) in zip([3,2,1,4], [1,2,3,4])
-#         println(fileout,@sprintf("ZIAENG, %d, 0.00, 0.00, %10.5f, 0.00, 1.00,  %10.5f, ZIAENG, 1.0", j, EIs[i], mfs[i]))
-#     end
-
-# end
-
 """
-    LTO(name, ac; fileout = stdout)
+LTO(name, ac; fileout = stdout)
 Prints out LTO EI(NOₓ) values
 """
 function LTO(name, ac; fileout = stdout)
 
-    #LTO values
+    # LTO values
     LTOpoints = [1.0, 0.85, 0.3, 0.07]
 
     LHV = ac.parg[igLHVfuel]
@@ -65,6 +19,7 @@ function LTO(name, ac; fileout = stdout)
 
     EIs = zeros(4)
     mfs = zeros(4)
+    Fns = zeros(4)
 
     icall = 2
     icool = 1
@@ -91,9 +46,11 @@ function LTO(name, ac; fileout = stdout)
             mdotf = ac.pared[ieff, iptest] * ac.pared[iemcore]
             P3_kPa = ac.pared[iept3, iptest]/1000.0
             T3_K   = ac.pared[ieTt3, iptest]
-            EINOx = EINOx3(ac, iptest)
+            # EINOx = EINOx3(ac, iptest)
+            EINOx = EINOx4(ac, iptest)
             EIs[i] = EINOx
             mfs[i] = mdotf
+            Fns[i] = F_total
 
             println(fileout, 
             @sprintf("%2d) %4.2f %15.5f %15.5f %15.5f %15.5f",
@@ -107,11 +64,11 @@ function LTO(name, ac; fileout = stdout)
                                         name, j, EIs[i], mfs[i], name))
         end
 
-        return EIs, mfs
+        return EIs, mfs, Fns
 end
 
 """
-    EINOx3(P3_kPa,T3_K, sp_humidity = 0.00634)
+EINOx3(P3_kPa,T3_K, sp_humidity = 0.00634)
 Returns the EI(NOₓ) for a CFM56 level engine.
 Uses a third order polynomial in T₃ 
 """
@@ -128,8 +85,15 @@ function EINOx3(P3_kPa, T3_K, sp_humidity = 0.00634)
 
     return EI_NOx
 end
+
+function EINOx3(ac::aircraft, ip::Int, sp_humidity = 0.00634)
+    P3_kPa = ac.pared[iept3, ip]/1000.0
+    T3_K   = ac.pared[ieTt3, ip]
+    EINOx3(P3_kPa, T3_K, sp_humidity)
+end
+
 """
-    EINOx4(P3_kPa, T3_K, sp_humidity = 0.00634)
+EINOx4(P3_kPa, T3_K, sp_humidity = 0.00634)
 Returns the EI(NOₓ) for a CFM56 level engine.
 Uses a fourth order polynomial which behaves a little better at low T₃ than the cubic
 """
@@ -142,12 +106,12 @@ function EINOx4(P3_kPa, T3_K, sp_humidity = 0.00634)
 
     H = -19.0*(sp_humidity - 0.00634)
 
-    EI_NOx = exp(H)*P3_kPa^0.4 * (a*T3_K^4 + b*T3_K^4 + c*T3_K^2 + d*T3_K + e)
+    EI_NOx = exp(H)*P3_kPa^0.4 * (a*T3_K^4 + b*T3_K^3 + c*T3_K^2 + d*T3_K + e)
     return EI_NOx
-end  # function EINOx4
+end
 
-function EINOx3(ac::aircraft, ip::Int, sp_humidity = 0.00634)
+function EINOx4(ac::aircraft, ip::Int, sp_humidity = 0.00634)
        P3_kPa = ac.pared[iept3, ip]/1000.0
        T3_K   = ac.pared[ieTt3, ip]
-       EINOx3(P3_kPa, T3_K, sp_humidity)
+       EINOx4(P3_kPa, T3_K, sp_humidity)
 end
