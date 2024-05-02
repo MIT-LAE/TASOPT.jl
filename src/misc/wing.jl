f6e545e3123be8663698d300c4bbbbc89b1c272c
Aditeya Shukla <aditeyashukla@gmail.com>
2024-05-02 11:53:56 -0400

    wing sections, a little bit of wing layout

--- src/misc/wing.jl
+++ src/misc/wing.jl
@@ -1,14 +1,25 @@
 #Wing class
 @kwdef mutable struct Wing
-    material::StructuralAlloy = StructuralAlloy("TASOPT-Al")
-    webs::StructuralMember = StructuralMember(material=material)
-    caps::StructuralMember = StructuralMember(material=material)
-    strut::StructuralMember = StructuralMember(material=material)
-    web_cap_strut::StructuralMember = StructuralMember(material=material)
-    web_cap_root::StructuralMember = StructuralMember(material=material)
+    # Layout
+    layout::WingLayout = WingLayout()
 
+    material::StructuralAlloy = StructuralAlloy("TASOPT-Al")
+
+    # Wing Sections
+    inboard::WingSection = WingSection(material)
+    outboard::WingSection = WingSection(material)
+
+    # Strut
+    strut::StructuralMember = StructuralMember(material=material)
     strut_area::Float64 = 0
     strut_length::Float64 = 0
-    web_thickness_strut_attach_point::Float64 = 0
-    cap_thickness_strut_attach_point::Float64 = 0
+end
+mutable struct WingSection
+    webs::StructuralMember = StructuralMember(material=material)
+    caps::StructuralMember = StructuralMember(material=material)
+    web_cap::StructuralMember = StructuralMember(material=material)
+end
+
+function WingSection(material)
+    return WingSection(StructuralMember(material=material), StructuralMember(material=material), StructuralMember(material=material))
 end
