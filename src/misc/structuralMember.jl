2c15d8b5aff4ce7180a43c5c0a45bcf127186953
Prashanth Prakash <prash@mit.edu>
2024-05-14 11:51:44 -0400

    spelling fix

--- src/misc/structuralMember.jl
+++ src/misc/structuralMember.jl
@@ -14,9 +14,9 @@ $TYPEDFIELDS
     weight::Float64 = 0 #OR Union{Float64, Nothing}
     """Stress [Pa]"""
     σ::Float64 = material.σmax
-    """Horizontal/Bending Stiffnesss [N m^2]""" 
+    """Horizontal/Bending Stiffness [N m^2]""" 
     EIh::Float64 = 0
-    """Vertical/Normal Stiffnesss [N m^2]"""
+    """Vertical/Normal Stiffness [N m^2]"""
     EIv::Float64 = 0
     """Torsional Rigidity [N m^2]"""
     GJ::Float64 = 0
@@ -26,7 +26,7 @@ $TYPEDFIELDS
     ρ::Float64 = material.ρ
     """Position [m]"""
     x::Float64 = 0
-    """Weight Laterial Distribution"""
+    """Weight Lateral Distribution"""
     dxW::Float64 = 0
     #Material = StructuralAlloy
 end
