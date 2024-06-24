4fa2e1d7342407aec6882cec1874c3690442ce63
Aditeya Shukla <aditeyashukla@gmail.com>
2024-06-24 12:04:34 -0400

    changed LTO (doesnt work)

--- src/mission/LTO.jl
+++ src/mission/LTO.jl
@@ -1,45 +1,59 @@
-function LTO(name, NPSS::Base.Process, ifirst, pare; fileout = stdout)
-nTshaft = parpt[ipt_nTshaft]
-#LTO values
-LTOpoints = [1.0, 0.85, 0.3, 0.07]
-LTOEINOx = zero(4)
-LTOmdotf = zero(4)
-LHV = parg[igLHVfuel]
-# Get Foo based on Tt4 or T/O thrust
-Tt4 = pare[ieTt4, ipstatic]
-Foo = pare[ieFe , ipstatic]
-ip = iptest
-EIs = zeros(4)
-mfs = zeros(4)
-NPSS_success, Foo, η, P, Hrej, heatexcess, 
-mdotf, deNOx, EINOx1, EINOx2, FAR, Tt3, OPR,
-Wc3, Tt41, EGT = NPSS_TEsysOD(NPSS, 0.0, 0.0,
-Foo, 0.0, 0.0, 0.0, 0.0, 0.0, ifirst, parg, parpt, pare, ip)
-println(fileout, "Aircraft LTO characteristics \t\t"*Dates.format(now(), DateFormat("u dd yyyy")))
-println(fileout, name)
-println(fileout, @sprintf("Foo = %.3f [kN]", Foo/1000))
-println(fileout, @sprintf("%2s) %4s %15s %15s %15s %15s %15s %15s %8s %16s", 
-"#", "TS", "Fn[kN]", "̇mf_total[kg/s]", "̇mf[kg/s]", "EI(NOₓ)[g/kg]", "Total_NOₓ[g/s]", "NOₓ[g/s]", "deNOₓ[%]", "JetAeqEI(NOₓ)" ))
+function LTO(name, pari,parg,para,pare; fileout = stdout)
 
-    for (i,TS) in enumerate(LTOpoints)
-        println(@sprintf("Running LTO point %.1f %%", TS*100))
-        NPSS_success, Ftotal, η, P, Hrej, heatexcess, 
-        mdotf, deNOx, EINOx1, EINOx2, FAR, Tt3, OPR, Wc3, Tt41, EGT = NPSS_TEsysOD(NPSS, 0.0, 0.0, Foo*TS, 0.0, 0.0, 0.0, 0.0, 0.0, ifirst, parg, parpt, pare, ip)
-        ifirst  = false
-
-        println(fileout, 
-        @sprintf("%2d) %4.2f %15.5f %15.5f %15.5f %15.5f %15.5f %15.5f %8.2f %16.5f",
-        i, TS, Ftotal/1000, mdotf, mdotf/nTshaft, EINOx2, EINOx2*mdotf, EINOx2*mdotf/nTshaft, deNOx*100, EINOx2*43/LHV))
-        EIs[i] = EINOx2
-        mfs[i] = mdotf/nTshaft
-    end
-    # for TS in LTOpoints[end:-1:1]
-    #     NPSS_success, Ftotal, η, P, Hrej, heatexcess, 
-    #     mdotf, deNOx, EINOx1, EINOx2, FAR, Tt3, OPR, Wc3, Tt41, EGT = NPSS_TEsysOD(NPSS, 0.0, 0.0, Foo*TS, 0.0, 0.0, 0.0, 0.0, 0.0, ifirst, parg, parpt, pare, ip)
-    # end
-    println(fileout, "------------------------- AEIC ENG_EI output --------------------------")
-    for (i,j) in zip([3,2,1,4], [1,2,3,4])
-        println(fileout,@sprintf("ZIAENG, %d, 0.00, 0.00, %10.5f, 0.00, 1.00,  %10.5f, ZIAENG, 1.0", j, EIs[i], mfs[i]))
+    #LTO values
+    LTOpoints = [1.0, 0.85, 0.3, 0.07]
+    LTOEINOx = zero(4)
+    LTOmdotf = zero(4)
+    LHV = parg[igLHVfuel]
+    # Get Foo based on Tt41 or T/O thrust
+    Tt41 = pare[ieTt41, ipstatic]
+    Foo = pare[ieFe , ipstatic] # This is for aircraft!
+    ip = iptest
+    
+    if Fn == 0.0
+        icall = 1 #Tt41 specified
+        # println("Tt41 specified mode")
+    else
+        icall = 2 #Fn specified
+        # println("Fn specified mode")
     end
 
-end
+    
+    icool = 0
+    inite1 = 1
+
+    _, _ = tfcalc!(pari, parg, view(para, :, ip), view(pare, :, ip), ip, icall, icool, inite1)
+    Ftotal = pare[ieFe, ip]
+    mdotf = pare[iemdotf, ip]
+    EINOx1  = pare[ieEINOx1, ip]
+
+    # NPSS_success, Foo, heatexcess, 
+    # mdotf, EINOx1, FAR, Mtip, Tblade, Tt3, OPR, BPR,
+    # Wc3, Tt41, EGT = NPSS_TFsysOD(NPSS, 0.0, 0.0,
+    # Foo, 0.0, mofft, Pofft, ifirst, parg, parpt, pare, ip)
+    
+    println(fileout, "Aircraft LTO characteristics \t\t"*Dates.format(now(), DateFormat("u dd yyyy")))
+    println(fileout, name)
+    println(fileout, @sprintf("Foo = %.3f [kN]", Foo/1000))
+    println(fileout, @sprintf("%2s) %4s %15s %15s %15s %15s %8s %16s", 
+    "#", "TS", "Fn[kN]", "̇mf[kg/s]", "EI(NOₓ)[g/kg]", "NOₓ[g/s]", "deNOₓ[%]", "JetAeqEI(NOₓ)" ))
+    
+        for (i,TS) in enumerate(LTOpoints)
+            # NPSS_success, Ftotal, heatexcess, 
+            #mdotf, EINOx1, FAR, Mtip, Tblade, Tt3, OPR, BPR, Wc3, Tt41, EGT = NPSS_TFsysOD(NPSS, 0.0, 0.0, Foo*TS, 0.0, mofft, Pofft, ifirst, parg, parpt, pare, ip)
+            _, _ = tfcalc!(pari, parg, view(para, :, ip), view(pare, :, ip), ip, icall, icool, inite1)
+            
+            Ftotal = pare[ieFe, ip]
+            mdotf = pare[iemdotf, ip]
+            EINOx1  = pare[ieEINOx1, ip]
+
+            ifirst  = false
+    
+            println(fileout, 
+            @sprintf("%2d) %4.2f %15.5f %15.5f %15.5f %15.5f",
+            i, TS, Ftotal/1000, mdotf, EINOx1, EINOx1*mdotf))
+        end
+    
+    
+    
+    end
