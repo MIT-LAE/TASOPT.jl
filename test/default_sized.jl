3ed30aced3ea37ae887319689d69cc30eb74a078
Aditeya Shukla <aditeyashukla@gmail.com>
2024-07-30 16:53:42 -0400

    added structures test

--- test/default_sized.jl
+++ test/default_sized.jl
@@ -3,69 +3,7 @@ parg = zeros(Float64, igtotal)
 parm = zeros(Float64, 2*imtotal)
 para = zeros(Float64, (iatotal, iptotal))
 pare = zeros(Float64, (ietotal, iptotal))
- 
-ac = load_default_model()
-# ------------------------------
-# Fuselage
-# ------------------------------
-fuse = ac.fuselage
-Weight = TASOPT.structures.Weight
-fuse.n_decks = 1.00000000000000000000 
-fuse.shell.weight = Weight(W = 20947.34834733798925299197 ) 
-fuse.shell.weight.r = [16.970781139221838, 0.0, 0.0]
-fuse.window.W = 11269.98000000000138243195 
-fuse.window_W_per_length = 435.00000000000000000000 
-fuse.insulation.W = 5018.72835762571048690006 
-fuse.insulation.r = [18.1356, 0.0, 0.0]
-fuse.insulation_W_per_area = 22.00000000000000000000 
-fuse.floor.weight.W = 13415.76301200039961258881 
-fuse.floor_W_per_area = 60.00000000000000000000 
-fuse.window.r = [18.1356, 0.0, 0.0]
-fuse.cone.weight = Weight(W = 5667.63293457306281197816 ) 
-fuse.bendingmaterial_h.weight = Weight(W = 14314.44887410087903845124 ) 
-fuse.bendingmaterial_v.weight = Weight(W = 7381.15962121803022455424 ) 
-fuse.weight = 198042.54742835607612505555 
-fuse.moment = 3620266.69662951119244098663 
-fuse.volume = 422.77743761859198912134 
-fuse.weight_frac_stringers = 0.34999999999999997780 
-fuse.weight_frac_frame = 0.25000000000000000000 
-fuse.weight_frac_skin_addl = 0.20000000000000001110 
-fuse.layout.nose_radius = 1.64999999999999991118 
-fuse.layout.tail_radius = 2.00000000000000000000 
-fuse.layout.x_nose = 0.00000000000000000000 
-fuse.layout.x_end = 37.79520000000000123919 
-fuse.layout.x_start_cylinder = 6.09600000000000008527 
-fuse.layout.x_end_cylinder = 29.56559999999999988063 
-fuse.layout.x_pressure_shell_fwd = 5.18160000000000042775 
-fuse.layout.x_pressure_shell_aft = 31.08960000000000079012 
-fuse.layout.x_cone_end = 35.66159999999999996589 
-fuse.bendingmaterial_h.weight.r = [ 31.85596337599880811808 ,0.0,0.0] 
-fuse.bendingmaterial_v.weight.r = [ 31.09855913065120702754 ,0.0,0.0] 
-fuse.layout.cross_section.radius = 1.95579999999999998295 
-fuse.layout.cross_section.bubble_lower_downward_shift = 0.38100000000000000533 
-fuse.layout.floor_depth = 0.12700000000000000178 
-fuse.layout.taper_tailcone = 0.29999999999999998890 
-fuse.ratio_young_mod_fuse_bending = 1.00000000000000000000 
-fuse.skin.thickness = 0.00105929143021018645 
-fuse.cone.thickness = 0.00122356331103976230 
-fuse.layout.thickness_webs = 0.00000000000000000000 
-fuse.floor.thickness = 0.00090615659813192320 
-fuse.shell.EIh = 2361319618.16573047637939453125 
-fuse.bendingmaterial_h.EIh = 24686136598.98891830444335937500 
-fuse.bendingmaterial_v.EIh = 24686136598.98891830444335937500 
-fuse.shell.EIv = 2317349005.39677524566650390625 
-fuse.bendingmaterial_h.EIv = 16837917350.80952644348144531250 
-fuse.bendingmaterial_v.EIv = 16837917350.80952644348144531250 
-fuse.shell.GJ = 1570840175.14735412597656250000 
-fuse.cone.GJ = 1814441570.09389352798461914062 
-fuse.APU.W = 7698.76022650000140856719 
-fuse.APU.r = [36.57600000000000051159,0.0,0.0] 
-fuse.seat.W = 21996.45779000000038649887 
-fuse.fixed.W = 13344.66600000000107684173 
-fuse.fixed.r = [2.13359999999999994102,0.0,0.0] 
-fuse.HPE_sys.r = [18.89760000000000061959,0.0,0.0] 
-fuse.HPE_sys.W = 0.01000000000000000021 
-fuse.added_payload.W = 76987.60226500000862870365 
+
 # ------------------------------
 # Flags  - stored in pari array:
 # ------------------------------
