098014f6707539b797580156bd14e96899199ea1
Prashanth Prakash <prash@mit.edu>
2024-10-01 14:16:03 -0400

    losses_1

--- src/propsys/PMSM.jl
+++ src/propsys/PMSM.jl
@@ -18,6 +18,10 @@ const μ₀ = 1.25663706127e-6 #N⋅A⁻² https://physics.nist.gov/cgi-bin/cuu/
     Ω::Float64 = 10e3
     """Slots per pole [-]"""
     Nsp::Int = 3
+    """Phases [-]"""
+    phases::Int = 3
+    """Phase resistance [Ω]"""
+    phase_resistance::Float64 = 0.0
     """Pole pairs [-]"""
     N_pole_pairs::Int = 8
     """Max rotor tip speed [m/s]"""
@@ -25,7 +29,7 @@ const μ₀ = 1.25663706127e-6 #N⋅A⁻² https://physics.nist.gov/cgi-bin/cuu/
     """Saturation Flux [Wb/m²]"""
     B_sat::Float64 = 1.8
     """Maximum Current Density [A/m²]"""
-    J_max::Float64= 1e6
+    J_max::Float64 = 1e6
     """Max ratio to saturation"""
     rB_sat::Float64 = 0.98
     """Stack length [m]"""
@@ -38,7 +42,7 @@ const μ₀ = 1.25663706127e-6 #N⋅A⁻² https://physics.nist.gov/cgi-bin/cuu/
     kₑ::Float64 = 32.183e-6 #https://arc.aiaa.org/doi/10.2514/6.2018-5026
     """Hysteresis loss coefficient [W/lbm/Hz]"""
     kₕ::Float64 = 10.664e-3 #https://arc.aiaa.org/doi/10.2514/6.2018-5026
-   
+
     """Mass of machine [kg]"""
     mass::Float64 = 0.0
 end
@@ -78,19 +82,23 @@ end
     thickness::Float64 = 0.02
     """Inner radius [m]"""
     Ri::Float64 = 0.01
+    """Flux density [Wb/m² = T]"""
+    B::Float64 = 1.0
     """Material"""
     material::AbstractMaterial
     """Mass [kg]"""
     mass::Float64 = 0.0
 end
 
-@kwdef mutable struct RotorGeometry 
+@kwdef mutable struct RotorGeometry
     """Rotor thickness [m]"""
     thickness::Float64 = 0.01
     """Inner radius [m]"""
     Ri::Float64 = 0.01
     """Outer radius [m]"""
     Ro::Float64 = 0.02
+    """Flux density [Wb/m² = T]"""
+    B::Float64 = 1.0
     """Material"""
     material::AbstractMaterial
     """Mass [kg]"""
@@ -104,6 +112,8 @@ end
     Ri::Float64 = 0.01
     """Outer radius [m]"""
     Ro::Float64 = 0.02
+    """Flux density [Wb/m² = T]"""
+    B::Float64 = 1.0
     """Material"""
     material::AbstractMaterial
     """Mass [kg]"""
@@ -158,23 +168,23 @@ function size_PMSM!(motor::Motor, shaft_speed::AbstractFloat, design_power::Abst
     teeth = motor.teeth
     windings = motor.windings
     shaft = motor.shaft
-    
-    Ω = shaft_speed*(2*π/60)
-    f = motor.N_pole_pairs*shaft_speed/60
+
+    Ω = shaft_speed * (2 * π / 60)
+    f = motor.N_pole_pairs * shaft_speed / 60
 
     #Outer radius of the magnet - subject to the highest rotational speeds
-    radius_gap = motor.U_max/Ω 
+    radius_gap = motor.U_max / Ω
 
     # Calculate maximum flux through the stator and rotor back iron
     # Really, this needs to be a constraint Bsbi ≤ Bsat (at the top level) rather 
     # than a prescribed setting...
-    B_stator = motor.rB_sat * motor.B_sat 
-    B_rotor = motor.rB_sat * motor.B_sat
-    
+    stator.B = motor.rB_sat * motor.B_sat
+    rotor.B = motor.rB_sat * motor.B_sat
+
     B_gap = airgap_flux(magnet.M, magnet.thickness, motor.airgap_thickness)
 
-    rotor.thickness = B_gap/B_rotor * π * radius_gap / (2*motor.N_pole_pairs)
-    stator.thickness = B_gap/B_stator * π * radius_gap / (2*motor.N_pole_pairs)
+    stator.thickness = B_gap / stator.B * π * radius_gap / (2 * motor.N_pole_pairs)
+    rotor.thickness = B_gap / rotor.B * π * radius_gap / (2 * motor.N_pole_pairs)
 
     rotor.Ro = radius_gap - magnet.thickness
     rotor.Ri = rotor.Ro - rotor.thickness
@@ -187,47 +197,47 @@ function size_PMSM!(motor::Motor, shaft_speed::AbstractFloat, design_power::Abst
     N_teeth = motor.Nsp * 2 * motor.N_pole_pairs
 
     A_teeth = N_teeth * teeth.thickness * teeth.width
-    A_slots = π*(teeth.Ri + stator.Ri)*teeth.thickness - A_teeth
-    A_slot = A_slots/N_teeth
-    λ = A_slots/(A_slots + A_teeth)
-    l_end_turns = π/(2*motor.N_pole_pairs) * teeth.Ri * (λ/sqrt(1 - λ^2))
+    A_slots = π * (teeth.Ri + stator.Ri) * teeth.thickness - A_teeth
+    A_slot = A_slots / N_teeth
+    λ = A_slots / (A_slots + A_teeth)
+    l_end_turns = π / (2 * motor.N_pole_pairs) * teeth.Ri * (λ / sqrt(1 - λ^2))
 
     #Armature reaction
     B_windings = μ₀ * motor.J_max * teeth.thickness
 
-    B_teeth = sqrt((B_gap/λ)^2 + B_windings^2)
-    if B_teeth > motor.B_sat
+    teeth.B = sqrt((B_gap / λ)^2 + B_windings^2)
+    if teeth.B > motor.B_sat
         @warn "Flux density in teeth exceeds saturation flux density"
     end
 
-    torque = design_power/Ω
-    slot_current = J_max * windings.kpf*A_slot #Update to be peak current
-    windings.N_turns = floor(windings.kpf * A_slot/windings.A_wire)
+    torque = design_power / Ω
+    slot_current = J_max * windings.kpf * A_slot #Update to be peak current
+    windings.N_turns = floor(windings.kpf * A_slot / windings.A_wire)
 
     #Assume 2/3 phases excited at any given time, implicitly assumes 3 phase machine
-    force_length = (2/3 * motor.Nsp * 2*motor.N_pole_pairs) * B_gap * slot_current
-    motor.l = torque /(force_length * radius_gap)
+    force_length = (2 / 3 * motor.Nsp * 2 * motor.N_pole_pairs) * B_gap * slot_current
+    motor.l = torque / (force_length * radius_gap)
 
     #Size shaft
     shaft.Ro = rotor.Ri
-    if shaft.Ro^4 > torque/shaft.material.τmax * 2 * shaft.Ro/π
-        shaft.Ri = (shaft.Ro^4 - torque/shaft.material.τmax * 2 * shaft.Ro/π)^0.25
+    if shaft.Ro^4 > torque / shaft.material.τmax * 2 * shaft.Ro / π
+        shaft.Ri = (shaft.Ro^4 - torque / shaft.material.τmax * 2 * shaft.Ro / π)^0.25
     else
-    # shaft can be solid as well
+        # shaft can be solid as well
         shaft.Ri = 0.0
     end
 
     #Masses
     rotor.mass = cross_sectional_area(rotor) * motor.l * rotor.material.density
     stator.mass = cross_sectional_area(stator) * motor.l * stator.material.density
-    magnet.mass = cross_sectional_area(radius_gap, rotor.Ro)* motor.l * magnet.material.density
+    magnet.mass = cross_sectional_area(radius_gap, rotor.Ro) * motor.l * magnet.material.density
     teeth.mass = A_teeth * motor.l * teeth.material.density
-   
-    total_winding_volume = A_slots * (motor.l + 2*l_end_turns)
-    windings.mass = windings.kpf * total_winding_volume * windings.conductor.material.density + 
-                    (1 - windings.kpf)* total_winding_volume * windings.insulator.material.density
-   
-    shaft.mass = cross_sectional_area(shaft) * (motor.l * shaft.l_extra)* shaft.material.density 
+
+    total_winding_volume = A_slots * (motor.l + 2 * l_end_turns)
+    windings.mass = windings.kpf * total_winding_volume * windings.conductor.material.density +
+                    (1 - windings.kpf) * total_winding_volume * windings.insulator.material.density
+
+    shaft.mass = cross_sectional_area(shaft) * (motor.l * shaft.l_extra) * shaft.material.density
 
     motor.mass = rotor.mass + stator.mass + magnet.mass + teeth.mass + windings.mass + shaft.mass
     #size_rotor!(rotor, R_gap)
@@ -235,14 +245,49 @@ function size_PMSM!(motor::Motor, shaft_speed::AbstractFloat, design_power::Abst
     #size_windings!()
 
     ## Calculate losses
-    
+    motor.phase_resistance = motor.Nsp / motor.phases * 2 * motor.N_pole_pairs *
+                             slot_resistance(motor.windings,
+                                 windings.kpf * A_slot,
+                                 motor.l + 2 * l_end_turns)
+
 
 end  # function size_PMSM!
 
 """
 """
+function calculate_losses(EM::Motor)
+
+end  # function calculate_losses
+
+"""
+"""
+function ohmic_loss(I::AbstractFloat, phase_resistance::AbstractFloat;
+    energized_phases::Int=2)
+    return I^2 * (energized_phases * phase_resistance)
+end  # function ohmic_loss
+
+"""
+"""
+function hysteresis_loss(motor::Motor)
+    return hysteresis_loss(motor.stator) + hysteresis_loss(motor.teeth)
+end  # function hysteresis_loss
+
+function hysteresis_loss(stator, f)
+    stator.mass * stator.material.kₕ * f *stator.B^rotor.material.α
+end
+    
+"""
+"""
+end
+function slot_resistance(wind::Windings, A, l)
+    resist = resistivity(wind.conductor, wind.T)
+    return resist * l / A
+end  # function slot_resistance
+
+"""
+"""
 function cross_sectional_area(Ro, Ri)
-    return π*(Ro^2 - Ri^2)
+    return π * (Ro^2 - Ri^2)
 end  # function cross_section
 
 # Might not need to define each of this and just let anything go
@@ -257,9 +302,9 @@ cross_sectional_area(R::ShaftGeometry) = cross_sectional_area(R.Ro, R.Ri)
 """
 function size_rotor!(rotor::RotorGeometry, R_gap::AbstractFloat)
     Ro = R_gap - magnet.thickness
-    rotor.thickness = (B_gap/B_rotor) * π * R_gap/(2p)
+    rotor.thickness = (B_gap / rotor.B) * π * R_gap / (2p)
     rotor.Ri = Ro
-    
+
 end  # function size_rotor!
 
 """
@@ -270,7 +315,7 @@ Simplified expression from a magnetic circuit analysis. Assumes no MMF drop
 across the steel. 
 """
 function airgap_flux(M, thickness, airgap)
-    B_gap = μ₀ * M * thickness/ (thickness + airgap)
+    B_gap = μ₀ * M * thickness / (thickness + airgap)
 end  # function airgap_flux
 
 end
