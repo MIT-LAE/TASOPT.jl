e92ff3054455a592784816df2f6690c0a3441b58
Wyatt Giroux <girouxw@mit.edu>
2024-10-08 16:18:00 -0400

    added lookup table maps; started bilinear implementation

---
+++ src/engine/tftbl.jl
@@ -0,0 +1,70 @@
+"""
+`tftbl.jl` loads in the E3 map data for bilinear interpolation in tfmap.jl
+"""
+using TOML
+
+struct compressorTbl
+    # ---- Independent variables
+    mb_map::AbstractArray{AbstractFloat}
+    pr_map::AbstractArray{AbstractFloat}
+
+    # ---- Design values
+    Nb_des::AbstractFloat
+    Rline_des::AbstractFloat # Currently unused
+    mb_des::AbstractFloat
+    pr_des::AbstractFloat
+    eff_is_des::AbstractFloat # Isentropic design efficiency
+
+    # ---- Lookup tables
+    Nb_tbl::AbstractArray{AbstractFloat}
+    Rline_tbl::AbstractArray{AbstractFloat}
+    eff_is_tbl::AbstractArray{AbstractFloat}
+    eff_poly_tbl::AbstractArray{AbstractFloat}
+end 
+
+
+E3fan_toml = TOML.parsefile("$(@__DIR__)/data/E3fan_tbl.toml")
+E3lpc_toml = TOML.parsefile("$(@__DIR__)/data/E3lpc_tbl.toml")
+E3hpc_toml = TOML.parsefile("$(@__DIR__)/data/E3hpc_tbl.toml")
+
+E3fan = compressorTbl(
+    E3fan_toml["input"]["mb"],
+    E3fan_toml["input"]["pr"],
+    E3fan_toml["design"]["Nb_des"],
+    E3fan_toml["design"]["Rline_des"],
+    E3fan_toml["design"]["mb_des"],
+    E3fan_toml["design"]["pr_des"],
+    E3fan_toml["design"]["eff_des"],
+    E3fan_toml["output"]["Nb"],
+    E3fan_toml["output"]["R"],
+    E3fan_toml["output"]["Ei"],
+    E3fan_toml["output"]["Ep"],
+)
+
+E3lpc = compressorTbl(
+    E3lpc_toml["input"]["mb"],
+    E3lpc_toml["input"]["pr"],
+    E3lpc_toml["design"]["Nb_des"],
+    E3lpc_toml["design"]["Rline_des"],
+    E3lpc_toml["design"]["mb_des"],
+    E3lpc_toml["design"]["pr_des"],
+    E3lpc_toml["design"]["eff_des"],
+    E3lpc_toml["output"]["Nb"],
+    E3lpc_toml["output"]["R"],
+    E3lpc_toml["output"]["Ei"],
+    E3lpc_toml["output"]["Ep"],
+)
+
+E3lpc = compressorTbl(
+    E3hpc_toml["input"]["mb"],
+    E3hpc_toml["input"]["pr"],
+    E3hpc_toml["design"]["Nb_des"],
+    E3hpc_toml["design"]["Rline_des"],
+    E3hpc_toml["design"]["mb_des"],
+    E3hpc_toml["design"]["pr_des"],
+    E3hpc_toml["design"]["eff_des"],
+    E3hpc_toml["output"]["Nb"],
+    E3hpc_toml["output"]["R"],
+    E3hpc_toml["output"]["Ei"],
+    E3hpc_toml["output"]["Ep"],
+)
