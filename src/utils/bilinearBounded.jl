a3c07845d9174bbb95c59bcabf91df93ba85107e
Wyatt Giroux <girouxw@mit.edu>
2024-10-15 12:35:03 -0400

    okay its working again

--- src/utils/bilinearBounded.jl
+++ src/utils/bilinearBounded.jl
@@ -55,129 +55,105 @@ function bilinearBoundedLookup(x, y, xGrid, yGrid, lookupGrid)
         return -1.0
     end
 
-    lookupIndices = [[xIndLow, yIndLow], [xIndLow, yIndUpp], [xIndUpp, yIndUpp], [xIndUpp, yIndLow]]
-
     # ---- Get function values at bounding points
     p1 = lookupGrid[yIndLow, xIndLow]
     p2 = lookupGrid[yIndUpp, xIndLow]
     p3 = lookupGrid[yIndUpp, xIndUpp]
     p4 = lookupGrid[yIndLow, xIndUpp]
-    pts = [p1, p2, p3, p4]
+
+    xLow, xUpp = xGrid[xIndLow], xGrid[xIndUpp]
+    yLow, yUpp = yGrid[yIndLow], yGrid[yIndUpp]
+
+    dx = xUpp - xLow
+    dy = yUpp - yLow
+
+    wxLow = (xUpp-x) / dx
+    wxUpp = (x-xLow) / dx
+    wyLow = (yUpp-y) / dy
+    wyUpp = (y-yLow) / dy
 
     # ---- Is each point in bounds? (true if in; false if out)
     p1in = !(abs(p1+1.0) < 1.0e-6)
     p2in = !(abs(p2+1.0) < 1.0e-6)
     p3in = !(abs(p3+1.0) < 1.0e-6)
     p4in = !(abs(p4+1.0) < 1.0e-6)
-    ptsIn = [p1in, p2in, p3in, p4in]
     boundSum = p1in + p2in + p3in + p4in
-    # println(boundSum)
-
-    filterIndIn = findall(x->x, ptsIn)
-    filterPts = pts[filterIndIn]
-    filterLookup = lookupIndices[filterIndIn]
-    # println(filterLookup)
 
     if boundSum == 0 # ---- Point lies outside map
         return -1.0
     end
 
     if boundSum == 1 # ---- Single point in bounds; return funciton value at that point
-        return filterPts[1]
+        if p1in
+            return p1
+        elseif p2in
+            return p2
+        elseif p3in
+            return p3
+        elseif p4in
+            return p4
+        else
+            # Something's gone wrong
+            return -1.0
+        end
     end 
 
     if boundSum == 2 # ---- Two points in bounds
+        
         # ---- Prepare useful quantities
-        xInd1 = filterLookup[1][1]
-        xInd2 = filterLookup[2][1]
-        yInd1 = filterLookup[1][2]
-        yInd2 = filterLookup[2][2]
-
-        x1 = xGrid[xInd1]
-        x2 = xGrid[xInd2]
-        y1 = yGrid[yInd1]
-        y2 = yGrid[yInd2]
-
-        dx = x2 - x1
-        dy = y2 - y1
-
-        # ---- Calculate weights based on point orientation
-        if filterInd == [1,2] || filterInd == [3,4] # ---- Vertical points in bounds
-            weight2 = abs((y - y1)/dy)
-            weight1 = abs((y - y2)/dy)
-        elseif filterInd == [2,3] || filterInd == [1,4] # ---- Horizontal points in bounds
-            weight2 = abs((x - x1)/dx)
-            weight1 = abs((x - x2)/dx)
-        else # ---- Diagonal points in bounds; this can never happen on a convex hull of points
+        if p1in && p2in # Left vertical
+            return p1 * wyLow + p2 * wyUpp
+        elseif p4in && p3in
+            return p4 * wyLow + p3 * wyUpp
+        elseif p2in && p3in
+            return p2 * wxLow + p3 * wxUpp
+        elseif p1in && p4in
+            return p1 * wxLow + p2 * wxUpp
+        else # ---- Diagonal points in bounds; this should never be able to happen
             return -1.0
         end
-
-        # ---- Return the linear interpolation of the two points
-        return filterPts[1] * weight1 + filterPts[2] * weight2
 
     elseif boundSum == 3 # ---- Three points in bounds
         # ---- Map each three point configuration to (corner, vertical, horizontal) indices on
         # ---- the filtered arrays
-        if filterIndIn == [1,2,3]
-            cornerInd = 2
-            vertInd = 1
-            horInd = 3
-        elseif filterIndIn == [2,3,4]
-            cornerInd = 2
-            vertInd = 3
-            horInd = 1
-        elseif filterIndIn == [1,2,4]
-            cornerInd = 1
-            vertInd = 2
-            horInd = 3
-        elseif filterIndIn == [1,3,4]
-            cornerInd = 3
-            vertInd = 2
-            horInd = 1
+        if (p1in && p2in) && p3in # Upper Left Cornered
+            fx = p2 * wxLow + p3 * wxUpp
+            fy = p2 * wyUpp + p1 * wyLow
+
+            fxy = fx * wyUpp + p1 * wyLow
+            fyx = fy * wxLow + p3 * wxUpp
+        elseif (p2in && p3in) && p4in # Upper Right Cornered
+            fx = p3 * wxUpp + p2 * wxLow
+            fy = p3 * wyUpp + p4 * wyLow
+
+            fxy = fx * wyUpp + p4 * wyLow
+            fyx = fy * wxUpp + p2 * wxLow
+        elseif (p1in && p2in) && p4in # Lower Left Cornered
+            fx = p1 * wxLow + p4 * wxUpp
+            fy = p1 * wyLow + p2 * wyUpp
+
+            fxy = fx * wyLow + p2 * wyUpp
+            fyx = fy * wxLow + p4 * wxUpp
+        elseif (p1in && p3in) && p4in # Lower Right Cornered
+            fx = p4 * wxUpp + p1 * wxLow
+            fy = p4 * wyLow + p3 * wyUpp
+
+            fxy = fx * wyLow + p3 * wyUpp
+            fyx = fy * wxUpp + p1 * wxLow
         else
             return -1.0
             # TODO raise error - this shouldn't be possible
         end
 
-        # ---- Get x and y values for each point
-        xCorner, yCorner = xGrid[ filterLookup[cornerInd][1] ], yGrid[ filterLookup[cornerInd][2] ]
-        yVert            =                                      yGrid[ filterLookup[vertInd][2]   ]
-        xHor             = xGrid[ filterLookup[horInd][1]    ]
-
-        # ---- Calculate the length and area infinitesimals
-        dx = abs(xHor  - xCorner)
-        dy = abs(yVert - yCorner)
-
-        # ---- Get the function value at each point
-        fCorner, fVert, fHor = filterPts[cornerInd], filterPts[vertInd], filterPts[horInd]
-
-        # Calculate the linear interpolation of function value along each axis
-        fx = abs(x - xHor)  / dx * fCorner + abs(x - xCorner) / dx * fHor
-        fy = abs(y - yVert) / dy * fCorner + abs(y - yCorner) / dy * fVert
-
-        # Calculate the linear interpolation of function and axis interpolation along the other axis
-        fxy = abs(y - yVert) / dy * fx + abs(y - yCorner) / dy * fVert
-        fyx = abs(x - xHor ) / dx * fy + abs(x - xCorner) / dx * xHor
-
-        # Average the two semi-bilinear interpolations to approximate the true function value
         return (fxy + fyx) / 2
 
     elseif boundSum == 4 # ---- Four points in bounds
-        x1, y1 = xGrid[filterLookup[1][1]], yGrid[filterLookup[1][2]]
-        x2, y2 = xGrid[filterLookup[2][1]], yGrid[filterLookup[2][2]]
-        x3, y3 = xGrid[filterLookup[3][1]], yGrid[filterLookup[3][2]]
-        x4, y4 = xGrid[filterLookup[4][1]], yGrid[filterLookup[4][2]]
-        
-        # ---- Get differentials assuming a regular grid
-        dx = abs(x3 - x2)
-        dy = abs(y2 - y1)
-
         # ---- Calculate the linear interpolation of function value along the `y`-axes
-        fy12 = abs(y - y2) / dy * p1 + abs(y - y1) / dy * p2
-        fy34 = abs(y - y4) / dy * p3 + abs(y - y3) / dy * p4
+        fy12 = p1 * wyLow + p2 * wyUpp
+        fy34 = p4 * wyLow + p3 * wyUpp
 
         # ---- Perform the bilinear interpolation
-        fapprox = (abs(x-x3) / dx) * fy12 + (abs(x-x1) / dx) * fy34
+        fapprox = fy12 * wxLow + fy34 * wxUpp
         return fapprox
     
     else # ---- Something has gone very wrong
