3dea662eba1ded015e75a2469b18ec050ede6203
Prashanth Prakash <prash@mit.edu>
2024-10-15 17:56:22 -0400

    doc strings + format

--- src/propsys/PMSM.jl
+++ src/propsys/PMSM.jl
@@ -186,7 +186,8 @@ function size_PMSM!(motor::Motor, shaft_speed::AbstractFloat, design_power::Abst
 
     # Calculate maximum flux through the stator and rotor back iron
     # Really, this needs to be a constraint Bsbi ≤ Bsat (at the top level) rather 
-    # than a prescribed setting...
+    # than a prescribed setting. 
+    # The higher Bsbi is the thinner the sbi, but higher Bsbi ⟹ higher core losses
     stator.B = motor.rB_sat * motor.B_sat
     rotor.B = motor.rB_sat * motor.B_sat
 
@@ -224,8 +225,9 @@ function size_PMSM!(motor::Motor, shaft_speed::AbstractFloat, design_power::Abst
     windings.N_turns = floor(windings.kpf * A_slot / windings.A_wire)
 
     #Assume 2 phases excited at any given time
-    motor.I = motor.Id = (2 / motor.phases * motor.Nsp * 2 * motor.N_pole_pairs) * slot_current
-    force_length = motor.Id * B_gap 
+    motor.I =
+        motor.Id = (2 / motor.phases * motor.Nsp * 2 * motor.N_pole_pairs) * slot_current
+    force_length = motor.Id * B_gap
     motor.l = torque / (force_length * radius_gap)
 
     #Size shaft
@@ -240,26 +242,31 @@ function size_PMSM!(motor::Motor, shaft_speed::AbstractFloat, design_power::Abst
     #Masses
     rotor.mass = cross_sectional_area(rotor) * motor.l * rotor.material.density
     stator.mass = cross_sectional_area(stator) * motor.l * stator.material.density
-    magnet.mass = cross_sectional_area(radius_gap, rotor.Ro) * motor.l * magnet.material.density
+    magnet.mass =
+        cross_sectional_area(radius_gap, rotor.Ro) * motor.l * magnet.material.density
     teeth.mass = A_teeth * motor.l * teeth.material.density
 
     total_winding_volume = A_slots * (motor.l + 2 * l_end_turns)
-    windings.mass = windings.kpf * total_winding_volume * windings.conductor.material.density +
-                    (1 - windings.kpf) * total_winding_volume * windings.insulator.material.density
+    windings.mass =
+        windings.kpf * total_winding_volume * windings.conductor.material.density +
+        (1 - windings.kpf) * total_winding_volume * windings.insulator.material.density
 
-    shaft.mass = cross_sectional_area(shaft) * (motor.l * shaft.l_extra) * shaft.material.density
+    shaft.mass =
+        cross_sectional_area(shaft) * (motor.l * shaft.l_extra) * shaft.material.density
 
-    motor.mass = rotor.mass + stator.mass + magnet.mass + teeth.mass + windings.mass + shaft.mass
+    motor.mass =
+        rotor.mass + stator.mass + magnet.mass + teeth.mass + windings.mass + shaft.mass
     #size_rotor!(rotor, R_gap)
     #size_stator!()
     #size_windings!()
 
     ## Calculate losses
     windings.T = motor.T # Set temperature
-    motor.phase_resistance = motor.Nsp / motor.phases * 2 * motor.N_pole_pairs *
-                             slot_resistance(motor.windings,
-                                 windings.kpf * A_slot,
-                                 motor.l + 2 * l_end_turns)
+    motor.phase_resistance =
+        motor.Nsp / motor.phases *
+        2 *
+        motor.N_pole_pairs *
+        slot_resistance(motor.windings, windings.kpf * A_slot, motor.l + 2 * l_end_turns)
 
 
 end  # function size_PMSM!
@@ -276,50 +283,76 @@ end  # function calculate_losses
 """
 
 Returns the power dissipated due to resistive heating assuming a certain 
-number of phases are energized at any given time. 
+number of phases are energized at any given time. By default assumes that 2 
+phases are energized at any given time.
 """
-function ohmic_loss(I::AbstractFloat, phase_resistance::AbstractFloat;
-    energized_phases::Int=2)
+function ohmic_loss(
+    I::AbstractFloat,
+    phase_resistance::AbstractFloat;
+    energized_phases::Int = 2,
+)
     return I^2 * (energized_phases * phase_resistance)
 end  # function ohmic_loss
 
 core_loss(motor) = hysteresis_loss(motor) + eddy_loss(motor)
 
 """
+$TYPEDSIGNATURES
+
+Calculates the hysteresis loss in the motor (i.e., in the rotor + the stator)
 """
 function hysteresis_loss(motor::Motor)
     return hysteresis_loss(motor.stator, motor.f) + hysteresis_loss(motor.teeth, motor.f)
 end  # function hysteresis_loss
 
+"""
+Calculates the hysteresis losses in steel. The steel needs to be of type
+[`ElectricSteel`](@ref).
+"""
 function hysteresis_loss(steel, f)
     return steel.mass * steel.material.kₕ * f * steel.B^steel.material.α
 end
 
 """
+$TYPEDSIGNATURES
+
+Calculates the eddy losses in the motor (i.e., due to the rotor + the stator)
 """
 function eddy_loss(motor::Motor)
     return eddy_loss(motor.stator, motor.f) + eddy_loss(motor.teeth, motor.f)
 end #function eddy_loss
 
-function eddy_loss(steel, f)
-    return steel.mass * steel.material.kₑ * f^2 * steel.B^2    
-end  # function eddy_loss
-    
 """
+Calculates the eddy losses in an electrical steel. The steel needs to be of type
+[`ElectricSteel`](@ref).
+"""
+function eddy_loss(steel, f)
+    return steel.mass * steel.material.kₑ * f^2 * steel.B^2
+end  # function eddy_loss
+
+"""
+Calculates the windage (i.e., air friction losses) for the rotor.
 """
 function windage_loss(motor::Motor, air)
     #air from IdealGases?
-    Re = motor.Ω * motor.radius_gap * motor.airgap_thickness * air.ρ/air.μ
-    Cf = 0.0725*Re^-0.2 #avg. skin friction approx. from fully turbulent flat plate
-    return Cf*π*air.ρ*motor.Ω^3 * motor.radius_gap^4 * motor.l
+    Re = motor.Ω * motor.radius_gap * motor.airgap_thickness * air.ρ / air.μ
+    Cf = 0.0725 * Re^-0.2 #avg. skin friction approx. from fully turbulent flat plate
+    return Cf * π * air.ρ * motor.Ω^3 * motor.radius_gap^4 * motor.l
 end  # function windage_loss
 
+"""
+Returns the slot resistance given a winding temperature and material.
+The winding material needs to be of type [`Conductor`](@ref).
+"""
 function slot_resistance(wind::Windings, A, l)
     resist = resistivity(wind.conductor, wind.T)
     return resist * l / A
 end  # function slot_resistance
 
 """
+$TYPEDSIGNATURES
+
+Returns the cross sectional area of an annulus with inner radius Rᵢ and outer radius Rₒ.
 """
 function cross_sectional_area(Ro, Ri)
     return π * (Ro^2 - Ri^2)
@@ -333,14 +366,14 @@ cross_sectional_area(R::StatorGeometry) = cross_sectional_area(R.Ro, R.Ri)
 cross_sectional_area(R::ShaftGeometry) = cross_sectional_area(R.Ro, R.Ri)
 
 
-"""
-"""
-function size_rotor!(rotor::RotorGeometry, R_gap::AbstractFloat)
-    Ro = R_gap - magnet.thickness
-    rotor.thickness = (B_gap / rotor.B) * π * R_gap / (2p)
-    rotor.Ri = Ro
+# """
+# """
+# function size_rotor!(rotor::RotorGeometry, R_gap::AbstractFloat)
+#     Ro = R_gap - magnet.thickness
+#     rotor.thickness = (B_gap / rotor.B) * π * R_gap / (2p)
+#     rotor.Ri = Ro
 
-end  # function size_rotor!
+# end  # function size_rotor!
 
 """
     airgap_flux(M, thickness, airgap)
@@ -353,5 +386,13 @@ across the steel.
 function airgap_flux(M, thickness, airgap)
     B_gap = μ₀ * M * thickness / (thickness + airgap)
 end  # function airgap_flux
+
+"""
+Linear model for magnet remanent flux as a function of temperature.
+This will be important if we model the TMS system along with the PMSM.
+"""
+function remanent_flux(magnet::AbstractMagnets, T)
+    magnet.remanent_flux * (1 - magnet.α * (T - (273.15 + magnet.Tbase)) / 100.0)
+end  # function remanent_flux
 
 end
