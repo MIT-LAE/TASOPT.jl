b0e560176247384b3e5f4d474b28ce6ce3a50136
Aditeya Shukla <aditeyashukla@gmail.com>
2024-10-29 18:29:40 -0400

    comments, markdown

--- example/example_gradient_based_opt.jl
+++ example/example_gradient_based_opt.jl
@@ -4,8 +4,10 @@ using Test
 using TASOPT
 include(__TASOPTindices__)
 
+# Set relative tolerance for Finite difference method
 epsilon = 1e-5
 
+# Set design variables
 input_params = [
     :(ac.parg[igAR]), 
     :(ac.para[iaCL,ipcruise1:ipcruise2,1]),
@@ -15,13 +17,16 @@ input_params = [
 ]
 params = map(p -> TASOPT.format_params(TASOPT.expr_to_string(p)), input_params)
 
+# Set box constraints
 lower      = [9.0 , 0.53, 25.0, 1400.0, 1.25]
 upper      = [11.0, 0.60, 30.0, 1650.0, 2.0 ] 
 initial    = [10.5, 0.57, 26.0, 1580.0, 1.685]
 
+# Load default model
 default_model = load_default_model() 
 size_aircraft!(default_model)
 
+# Objective and Constraint Functions
 function pfei_fn(ac)
     return ac.parm[imPFEI]
 end
@@ -43,11 +48,13 @@ function tt3_fn(ac)
     return maximum(ac.pare[ieTt3, :, 1])/maxtt3
 end
 
+# Create an array that stores above functions
+# This is used when Memoizing the results 
 con_f_arr = [
     pfei_fn, wfuel_fn, span_fn, tt3_fn, gtoc_fn
 ]
 
-# Function that sizes aircraft and returns both objective function and gradient
+# Function that sizes aircraft and returns both objective value and constraint values
 function sizing_ac(x::T...) where {T<:Real}
     ac = deepcopy(default_model)
     # Set params
@@ -56,7 +63,6 @@ function sizing_ac(x::T...) where {T<:Real}
         TASOPT.setNestedProp!(ac, field_path, x_i, index)
     end
 
-    # size and get objective value and constraint
     try
         size_aircraft!(ac,printiter=false)
         return [con_f_arr[i](ac) for i in 1:length(con_f_arr)]
@@ -67,6 +73,7 @@ function sizing_ac(x::T...) where {T<:Real}
     
 end
 
+# Function that uses TASOPT Sensitivity module to get Finite difference of both objective and constraints
 function gradient_all(x::T...) where {T<:Real}
     ac = deepcopy(default_model)
     # Set params
@@ -77,16 +84,10 @@ function gradient_all(x::T...) where {T<:Real}
     return TASOPT.get_sensitivity(input_params; model_state=ac, eps=epsilon, optimizer=true, f_out_fn=con_f_arr)
 end
 
-"""
-    memoize(foo::Function, n_outputs::Int)
 
-Take a function `foo` and return a vector of length `n_outputs`, where element
-`i` is a function that returns the equivalent of `foo(x...)[i]`.
-
-To avoid duplication of work, cache the most-recent evaluations of `foo`.
-Because `foo_i` is auto-differentiated with ForwardDiff, our cache needs to
-work when `x` is a `Float64` and a `ForwardDiff.Dual`.
-"""
+# Memoization Functions
+# The below functions Take a function `foo` and return a vector of length `n_outputs`, where element
+# `i` is a function that returns the equivalent of `foo(x...)[i]`.
 function memoize(foo::Function, n_outputs::Int)
     last_x, last_f = nothing, nothing
     function foo_i(i, x::T...) where {T<:Real}
@@ -113,26 +114,34 @@ function memoize_sensitivity(foo::Function, n_outputs::Int)
     return [(x...) -> foo_s!(i, x...) for i in 1:n_outputs]
 end
 
+# Set memoization functions
 memoized_size_ac = memoize(sizing_ac, length(con_f_arr))
 memoized_fd_ac = memoize_sensitivity(gradient_all, length(con_f_arr))
 
+# Start optimization setup
 model = Model(Ipopt.Optimizer)
-# set_silent(model)
+
+# Scaling parameters for faster convergence
 set_optimizer_attribute(model, "tol", 1e-3)
 set_optimizer_attribute(model, "nlp_scaling_method", "gradient-based")
 set_optimizer_attribute(model, "obj_scaling_factor", 1.0)
 set_optimizer_attribute(model, "nlp_scaling_max_gradient", Float64(1))
 set_optimizer_attribute(model, "nlp_scaling_min_value", 1e-8)
 
+# Set design variables
 @variable(model, lower[i] <= x[i=1:length(initial)] <= upper[i], start=initial[i])
 
+# Set objective value and constraint functions
 @operator(model, f_size_ac, length(initial), memoized_size_ac[1],memoized_fd_ac[1])
 @operator(model, f_con_wfuel, length(initial), memoized_size_ac[2],memoized_fd_ac[2])
 @operator(model, f_con_b, length(initial), memoized_size_ac[3],memoized_fd_ac[3])
 @operator(model, f_con_tt3, length(initial), memoized_size_ac[4],memoized_fd_ac[4])
 @operator(model, f_con_gtoc, length(initial), memoized_size_ac[5],memoized_fd_ac[5])
 
+# Objective function
 @objective(model, Min, f_size_ac(x...))
+
+# Constraints
 @constraint(model, f_con_wfuel(x...) <= 1)
 @constraint(model, f_con_b(x...) <= 1)
 @constraint(model, f_con_tt3(x...) <= 1)
