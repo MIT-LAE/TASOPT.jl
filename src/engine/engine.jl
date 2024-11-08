229540ae54deb48afd63a68ca802ad23e9cd16ac
Nicolas Gomez <ngomezve@mit.edu>
2024-11-08 11:41:35 -0500

    updated engine to load functions

--- src/engine/engine.jl
+++ src/engine/engine.jl
@@ -6,10 +6,12 @@ module engine
 
 using NLopt
 using Roots
+using NLsolve
 using LinearAlgebra
 
 export tfcalc!, mcool, Tmcalc, gas_tset, gaschem
 export tfweight, ddct, ddat, gct, gat, tfsize!, Ncmap, ecmap, Ncmap1, ecmap1, etmap, Pimap, tfoper!
+export ductedfanoper!, ductedfansize!, ductedfancalc!, ductedfanweight
 
 export gassum, gassumd, gas_prat, gas_delh, gas_delhd, gas_burn, gas_burnd, gas_mach, gas_machd, gas_mass, gasfuel, fuelLHV, gasPr
 export hxdesign!, hxweight, resetHXs
@@ -31,5 +33,9 @@ include("turbofan/tfcalc.jl")
 include("turbofan/tfweight.jl")
 include("hxfun.jl")
 include("PEMfuelcell.jl")
+include("ducted_fan/ductedfancalc.jl")
+include("ducted_fan/ductedfansize.jl")
+include("ducted_fan/ductedfanoper.jl")
+include("ducted_fan/ductedfanweight.jl")
 
 end
