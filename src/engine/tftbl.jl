150b89f2032d46483bd1b9f66414ba8013d035cd
Wyatt Giroux <girouxw@mit.edu>
2024-11-26 15:50:10 -0500

    Refined bilinear method; down to 100ns timing; faster than drela method

--- src/engine/tftbl.jl
+++ src/engine/tftbl.jl
@@ -3,18 +3,88 @@
 """
 struct compressorTbl
     # ---- Design values
-    Nb_des::AbstractFloat
-    Rline_des::AbstractFloat # Currently unused
-    mb_des::AbstractFloat
-    pr_des::AbstractFloat
-    eff_is_des::AbstractFloat # Isentropic design efficiency
+    Nb_des::Float64
+    Rline_des::Float64 # Currently unused
+    mb_des::Float64
+    pr_des::Float64
+    eff_is_des::Float64 # Isentropic design efficiency
 
-    # ---- Interpolators (Nominal and Gradient)
-    Nc_itp::natNeigh.NaturalNeighboursInterpolant  
-    Nc_grad::natNeigh.NaturalNeighboursDifferentiator    
-    eff_itp::natNeigh.NaturalNeighboursInterpolant  
-    eff_grad::natNeigh.NaturalNeighboursDifferentiator    
+    # ---- mb and PR grids and fineness variables
+    mbGrid::Vector{Float64}
+    prGrid::Vector{Float64}
+    dm::Float64
+    dp::Float64
+    Nm::Int
+    Np::Int
+
+    # ---- Nc grids
+    Nb_nom::Matrix{Float64}
+    Nb_mb::Matrix{Float64}
+    Nb_pr::Matrix{Float64}
+
+    # ---- Eff grids
+    Eff_nom::Matrix{Float64}
+    Eff_mb::Matrix{Float64}
+    Eff_pr::Matrix{Float64}
+
+    # ---- Hash
+    hash_val::Integer
 end 
+
+
+function compTbl_to_TOML(compressor::compressorTbl, saveName)
+    # Convert the struct fields to a dictionary
+    compressor_dict = Dict(
+        :Nb_des => compressor.Nb_des,
+        :Rline_des => compressor.Rline_des,
+        :mb_des => compressor.mb_des,
+        :pr_des => compressor.pr_des,
+        :eff_is_des => compressor.eff_is_des,
+        :mbGrid => compressor.mbGrid,
+        :prGrid => compressor.prGrid,
+        :dm => compressor.dm,
+        :dp => compressor.dp,
+        :Nm => compressor.Nm,
+        :Np => compressor.Np,
+        :Nb_nom => [compressor.Nb_nom[i, :] for i in 1:size(compressor.Nb_nom, 1)],
+        :Nb_mb => [compressor.Nb_mb[i, :] for i in 1:size(compressor.Nb_mb, 1)],
+        :Nb_pr => [compressor.Nb_pr[i, :] for i in 1:size(compressor.Nb_pr, 1)],
+        :Eff_nom => [compressor.Eff_nom[i, :] for i in 1:size(compressor.Eff_nom, 1)],
+        :Eff_mb => [compressor.Eff_mb[i, :] for i in 1:size(compressor.Eff_mb, 1)],
+        :Eff_pr => [compressor.Eff_pr[i, :] for i in 1:size(compressor.Eff_pr, 1)],
+        :hash_val => compressor.hash_val
+    )
+
+    open(joinpath(__TASOPTroot__,"engine/data/",saveName), "w") do io
+        TOML.print(io, compressor_dict)
+    end
+end # compTbl_to_TOML
+
+
+function dict_to_compTbl(compressor_dict)
+    compressor_struct = compressorTbl(
+        compressor_dict["Nb_des"],
+        compressor_dict["Rline_des"],
+        compressor_dict["mb_des"],
+        compressor_dict["pr_des"],
+        compressor_dict["eff_is_des"],
+        compressor_dict["mbGrid"],
+        compressor_dict["prGrid"],
+        compressor_dict["dm"],
+        compressor_dict["dp"],
+        compressor_dict["Nm"],
+        compressor_dict["Np"],
+        transpose(hcat(compressor_dict["Nb_nom"]...)),
+        transpose(hcat(compressor_dict["Nb_mb"]...)),
+        transpose(hcat(compressor_dict["Nb_pr"]...)),
+        transpose(hcat(compressor_dict["Eff_nom"]...)),
+        transpose(hcat(compressor_dict["Eff_mb"]...)),
+        transpose(hcat(compressor_dict["Eff_pr"]...)),
+        compressor_dict["hash_val"]
+    )
+    
+    return compressor_struct
+end # dict_to_compTbl
 
 
 function gen2DGrid(itp, xLow, xHigh, yLow, yHigh, Nx, Ny; method=natNeigh.Sibson(1))
@@ -84,10 +154,11 @@ for correected speed and efficiency.
     - `mapStruct`: compressorTbl representation of the map
 
 """
-function create_map_struct(map_name::String; method::natNeigh.AbstractInterpolator=natNeigh.Sibson(), Nres::Int=50, Rres::Int=50)
+function create_map_struct(map_name::String; method::natNeigh.AbstractInterpolator=natNeigh.Sibson(), Nres::Int=250, Rres::Int=250, Mres::Int=100, Pres::Int=100)
     # Load map data from TOML file
     println("Generatring Map: " * map_name)
     map = TOML.parsefile(joinpath(__TASOPTroot__,"engine/data/",map_name))
+    hash_val = hash(map)
 
     # Need to specify each point in the N-R plane (i.e. all combinations of N_i and R_i)
     # To do so, create a vector with all x (N) components and a vector with all y (R) components
@@ -123,12 +194,18 @@ function create_map_struct(map_name::String; method::natNeigh.AbstractInterpolat
     N_interpolator::natNeigh.NaturalNeighboursInterpolant = natNeigh.interpolate(M_testVec, P_testVec, N_testVec; derivatives=true)
     E_interpolator::natNeigh.NaturalNeighboursInterpolant = natNeigh.interpolate(M_testVec, P_testVec, E_testVec; derivatives=true)
 
-    # Create the gradient calculators for each output variable
-    println("Creating final gradients")
-    N_gradients::natNeigh.NaturalNeighboursDifferentiator = natNeigh.differentiate(N_interpolator, 1)
-    E_gradients::natNeigh.NaturalNeighboursDifferentiator = natNeigh.differentiate(E_interpolator, 1)
+    # Use the interpolators to create a regular grid of N and efficiency values
+    println("Gridding data")
+    N_gridded, Mg, Pg, dm, dp = gen2DGrid(N_interpolator, minimum(Mvec), maximum(Mvec), minimum(Pvec), maximum(Pvec), Mres, Pres; method=natNeigh.Sibson(1))
+    E_gridded, _,  _,  _,  _  = gen2DGrid(E_interpolator, minimum(Mvec), maximum(Mvec), minimum(Pvec), maximum(Pvec), Mres, Pres; method=natNeigh.Sibson(1)) 
+
+    # Calculate the partial derivatives of N and efficiency to mass flow and pressure ratio on the same grid
+    println("Calculating partials")
+    dNdm, dNdp = gen2DDerivatives(N_gridded, dm, dp, Mres, Pres)
+    dEdm, dEdp = gen2DDerivatives(E_gridded, dm, dp, Mres, Pres)
 
     # Get the design quantities for each relevant input/output
+    println("Getting design values")
     Nb_des::Float64 = map["design"]["des_Nc"]
     Rline_des::Float64 = map["design"]["des_Rline"]
     
@@ -140,16 +217,26 @@ function create_map_struct(map_name::String; method::natNeigh.AbstractInterpolat
     eff_des::Float64 = map["characteristic"]["isen_efficiency"][N_ind][R_ind]
 
     # Construct the compressorTbl struct for the given map
+    println("Constructing compressorTbl")
     mapStruct = compressorTbl(
         Nb_des,
         Rline_des,
         mb_des,
         pr_des,
         eff_des,
-        N_interpolator,
-        N_gradients,
-        E_interpolator,
-        E_gradients
+        Mg,
+        Pg,
+        dm,
+        dp,
+        Mres,
+        Pres,
+        N_gridded,
+        dNdm,
+        dNdp,
+        E_gridded,
+        dEdm,
+        dEdp,
+        hash_val,
     )
 
     println("Done Generating\n\n")
@@ -157,18 +244,39 @@ function create_map_struct(map_name::String; method::natNeigh.AbstractInterpolat
 end # create_map_struct
 
 
-if isfile(joinpath(__TASOPTroot__,"engine/data/E3fan_gridded.toml"))
-    fanDict = TOML.parsefile(joinpath(__TASOPTroot__,"engine/data/E3fan_gridded.toml"))
-else
-    fanDict = create_map_struct("E3fan.toml"; method=natNeigh.Triangle(0; allow_cache=false), Nres=250, Rres=250)
-end
+function checkMapHash(gridded_map_name, data_map_name)
+    println("Checking hash for"*data_map_name)
+    genComp = true
+    compDict = Dict()
+    cacheDict = Dict()
+    if isfile(joinpath(__TASOPTroot__,"engine/data/",gridded_map_name))
+        compDict = TOML.parsefile(joinpath(__TASOPTroot__,"engine/data/",data_map_name))
+        cacheDict = TOML.parsefile(joinpath(__TASOPTroot__,"engine/data/",gridded_map_name))
+        if hash(compDict) == cacheDict["hash_val"]
+            genComp = false
+        end
+    end
+
+    return genComp, cacheDict
+end # checkMapHash
 
 
+function map_startup(gridded_map_name, data_map_name, map_gridding_method; Nres=250, Rres=250, Mres=100, Pres=100)
+    genComp, compDict = checkMapHash(gridded_map_name, data_map_name)
+
+    if genComp
+        # Generate new fan data and save to a file
+        compStruct = create_map_struct(data_map_name; method=map_gridding_method, Nres=Nres, Rres=Rres, Mres=Mres, Pres=Pres)
+        compTbl_to_TOML(compStruct, gridded_map_name)    
+    else
+        compStruct = dict_to_compTbl(compDict)
+    end
+
+    return compStruct
+end # map_startup
 
 
-
-
-const E3lpc = create_map_struct("E3lpc.toml"; method=natNeigh.Sibson(0), Nres=50, Rres=50)
-const E3hpc = create_map_struct("E3hpc.toml"; method=natNeigh.Triangle(0; allow_cache=false), Nres=25, Rres=25)
-
-
+# Create compressorTbl objects as constants to prevent re-generating all the maps every run
+const E3fan = map_startup("E3fan_gridded.toml", "E3fan.toml", natNeigh.Triangle(0; allow_cache=false); Nres=300, Rres=300, Mres=100, Pres=100)
+const E3lpc = map_startup("E3lpc_gridded.toml", "E3lpc.toml", natNeigh.Sibson(0);                      Nres=300, Rres=300, Mres=100, Pres=100)
+const E3hpc = map_startup("E3hpc_gridded.toml", "E3hpc.toml", natNeigh.Triangle(0; allow_cache=false); Nres=300, Rres=300, Mres=100, Pres=100)
