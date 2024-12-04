221338ba5e595ba577cd8a6c4858691e03cb6845
Wyatt Giroux <girouxw@mit.edu>
2024-12-04 00:25:29 -0500

    derivative noise was due to differing scales between mass flow and PR

--- src/engine/tftbl.jl
+++ src/engine/tftbl.jl
@@ -160,13 +160,25 @@ function create_map_struct(map_name::String; method::natNeigh.AbstractInterpolat
     map = TOML.parsefile(joinpath(__TASOPTroot__,"engine/data/",map_name))
     hash_val = hash(map)
 
+    # Get the design quantities for each relevant input/output
+    println("Getting design values")
+    Nb_des::Float64 = map["design"]["des_Nc"]
+    Rline_des::Float64 = map["design"]["des_Rline"]
+    
+    N_ind::Int = findall(x->isapprox(x,Nb_des), map["grid"]["grid_Nc"])[1]
+    R_ind::Int = findall(x->isapprox(x,Rline_des), map["grid"]["grid_Rline"])[1]
+
+    mb_des::Float64  = map["characteristic"]["massflow"][N_ind][R_ind]
+    pr_des::Float64  = map["characteristic"]["pressure_ratio"][N_ind][R_ind]
+    eff_des::Float64 = map["characteristic"]["isen_efficiency"][N_ind][R_ind]
+
     # Need to specify each point in the N-R plane (i.e. all combinations of N_i and R_i)
     # To do so, create a vector with all x (N) components and a vector with all y (R) components
     Nvec::Vector{Float64} = vec([i for i in map["grid"]["grid_Nc"], _ in map["grid"]["grid_Rline"]])
     Rvec::Vector{Float64} = vec([j for _ in map["grid"]["grid_Nc"], j in map["grid"]["grid_Rline"]])
 
     # Load in the mass flow, pressure ratio, and isentropic efficiency values in the same order as the points are specified above
-    Mvec::Vector{Float64} = vec([map["characteristic"]["massflow"][i][j]        for i in 1:length(map["grid"]["grid_Nc"]), j in 1:length(map["grid"]["grid_Rline"])])
+    Mvec::Vector{Float64} = vec([map["characteristic"]["massflow"][i][j]/mb_des for i in 1:length(map["grid"]["grid_Nc"]), j in 1:length(map["grid"]["grid_Rline"])])
     Pvec::Vector{Float64} = vec([map["characteristic"]["pressure_ratio"][i][j]  for i in 1:length(map["grid"]["grid_Nc"]), j in 1:length(map["grid"]["grid_Rline"])])
     Evec::Vector{Float64} = vec([map["characteristic"]["isen_efficiency"][i][j] for i in 1:length(map["grid"]["grid_Nc"]), j in 1:length(map["grid"]["grid_Rline"])])
 
@@ -199,22 +211,13 @@ function create_map_struct(map_name::String; method::natNeigh.AbstractInterpolat
     N_gridded, Mg, Pg, dm, dp = gen2DGrid(N_interpolator, minimum(Mvec), maximum(Mvec), minimum(Pvec), maximum(Pvec), Mres, Pres; method=natNeigh.Sibson(1))
     E_gridded, _,  _,  _,  _  = gen2DGrid(E_interpolator, minimum(Mvec), maximum(Mvec), minimum(Pvec), maximum(Pvec), Mres, Pres; method=natNeigh.Sibson(1)) 
 
+    Mg = Mg .* mb_des
+    dm = dm  * mb_des
+
     # Calculate the partial derivatives of N and efficiency to mass flow and pressure ratio on the same grid
     println("Calculating partials")
     dNdm, dNdp = gen2DDerivatives(N_gridded, dm, dp, Mres, Pres)
     dEdm, dEdp = gen2DDerivatives(E_gridded, dm, dp, Mres, Pres)
-
-    # Get the design quantities for each relevant input/output
-    println("Getting design values")
-    Nb_des::Float64 = map["design"]["des_Nc"]
-    Rline_des::Float64 = map["design"]["des_Rline"]
-    
-    N_ind::Int = findall(x->isapprox(x,Nb_des), map["grid"]["grid_Nc"])[1]
-    R_ind::Int = findall(x->isapprox(x,Rline_des), map["grid"]["grid_Rline"])[1]
-
-    mb_des::Float64  = map["characteristic"]["massflow"][N_ind][R_ind]
-    pr_des::Float64  = map["characteristic"]["pressure_ratio"][N_ind][R_ind]
-    eff_des::Float64 = map["characteristic"]["isen_efficiency"][N_ind][R_ind]
 
     # Construct the compressorTbl struct for the given map
     println("Constructing compressorTbl")
@@ -276,9 +279,9 @@ function map_startup(gridded_map_name, data_map_name, map_gridding_method; Nres=
 end # map_startup
 
 
-N, R, M, P = 300, 500, 250, 250
+N, R, M, P = 100, 100, 100, 100
 
 # Create compressorTbl objects as constants to prevent re-generating all the maps every run
-const E3fan = map_startup("E3fan_gridded.toml", "E3fan.toml", natNeigh.Triangle(0; allow_cache=false); Nres=N, Rres=R, Mres=M, Pres=P)
-const E3lpc = map_startup("E3lpc_gridded.toml", "E3lpc.toml", natNeigh.Sibson(0);                      Nres=N, Rres=R, Mres=M, Pres=P)
-const E3hpc = map_startup("E3hpc_gridded.toml", "E3hpc.toml", natNeigh.Triangle(0; allow_cache=false); Nres=N, Rres=R, Mres=M, Pres=P)
+const E3fan = map_startup("E3fan_gridded.toml", "E3fan.toml", natNeigh.Sibson(1); Nres=N, Rres=R, Mres=M, Pres=P)
+const E3lpc = map_startup("E3lpc_gridded.toml", "E3lpc.toml", natNeigh.Sibson(1);                      Nres=N, Rres=R, Mres=M, Pres=P)
+const E3hpc = map_startup("E3hpc_gridded.toml", "E3hpc.toml", natNeigh.Sibson(1); Nres=N, Rres=R, Mres=M, Pres=P)
