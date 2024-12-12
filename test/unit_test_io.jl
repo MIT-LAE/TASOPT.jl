38158c7d6b36f429a7b0b9d3f22852679a28b207
Jonas J. Gonzalez <argonaut@mit.edu>
2024-12-12 15:56:13 -0500

    properly commenting io tests. will show as broken, some as error "unexpected pass"

--- test/unit_test_io.jl
+++ test/unit_test_io.jl
@@ -1,5 +1,5 @@
 #tests io functionalities
-@test_skip @testset "io" verbose=true begin
+@testset "io" verbose=true begin
 #A: readable TOML saves
     #check that the default model is sized identically via MTOW
         #when round-tripped via model save and read
@@ -12,33 +12,40 @@
     size_aircraft!(ac_def, Ldebug=false, printiter=false, saveOD=false)
     size_aircraft!(ac_reread, Ldebug=false, printiter=false, saveOD=false)
 
-    @test ac_def.parg[igWMTO] ≈ ac_reread.parg[igWMTO]
+    @test_skip ac_def.parg[igWMTO] ≈ ac_reread.parg[igWMTO]
     rm(filepath_rewrite)
 
     #check via MTOW that changing an important parameter survives the save
     # and changes the solution
-    ac_nopay = load_default_model()
-    ac_nopay.parm[imWpay] = 1 #N
+    ac_lopay = load_default_model()
+    ac_lopay.parm[imWpay] = ac_def.parm[imWperpax, 1] * 30 #thirty pax in N
     filepath_nopay = joinpath(TASOPT.__TASOPTroot__, "../test/iotest_nopay.toml")
-    save_aircraft_model(ac_nopay, filepath_nopay)
+    save_aircraft_model(ac_lopay, filepath_nopay)
 
-    ac_nopay_reread = read_aircraft_model(filepath_nopay)
-    size_aircraft!(ac_nopay, Ldebug=false, printiter=false, saveOD=false)
-    size_aircraft!(ac_nopay_reread, Ldebug=false, printiter=false, saveOD=false)
+    ac_lopay_reread = read_aircraft_model(filepath_nopay)
+    size_aircraft!(ac_lopay, Ldebug=false, printiter=false, saveOD=false)
+    size_aircraft!(ac_lopay_reread, Ldebug=false, printiter=false, saveOD=false)
     
-    @test ac_nopay.parg[igWMTO] ≈ ac_nopay_reread.parg[igWMTO]
-    @test !(ac_nopay.parg[igWMTO] ≈ ac_def.parg[igWMTO])
+    @test ac_lopay.parg[igWMTO] ≈ ac_lopay_reread.parg[igWMTO]
+    @test !(ac_lopay.parg[igWMTO] ≈ ac_def.parg[igWMTO])
     rm(filepath_nopay)
 
 #B: quicksaves and loads
+
+    #!!!
+    #TODO: quicksaves need to be updated to handle struct formats
+    # the quicksaves largely work, but they haven't been checked/properly tested
+    #until then, these need to be disabled
+
+    #=
     #test that quicksave/load roundtrip default aircraft sizes identically to default load
     filepath_quick = joinpath(TASOPT.__TASOPTroot__, "../test/iotest_quick.toml")
     quicksave_aircraft(load_default_model(), filepath = filepath_quick)
     ac_quick = quickload_aircraft(filepath_quick)
-    size_aircraft!(ac_quick, Ldebug=false, printiter=false, saveOD=false)
+    size_aircraft!(ac_quick, Ldebug=true, printiter=true, saveOD=false)
 
-    @test ac_quick.parg[igWMTO] ≈ ac_def.parg[igWMTO]
-    rm(filepath_quick)
+    @test_broken ac_quick.parg[igWMTO] ≈ ac_def.parg[igWMTO]
+    # rm(filepath_quick)
 
     #check via MTOW that changing an important parameter survives the quicksave
     # and changes the solution
@@ -48,10 +55,16 @@
 
     ac_quick_nopay_reread = quickload_aircraft(filepath_quick_nopay)
     size_aircraft!(ac_quick_nopay_reread, Ldebug=false, printiter=false, saveOD=false)
-    @test ac_quick_nopay_reread.parg[igWMTO] ≈ ac_nopay.parg[igWMTO]
+    @test_broken ac_quick_nopay_reread.parg[igWMTO] ≈ ac_lopay.parg[igWMTO]
     rm(filepath_quick_nopay)
+    =#
 
 #C: outputs to .csv
+
+    #!!!
+    #TODO: these work! (hence "unexpected pass") but they need to be updated to handle struct formats
+    #currently, those parameters are glossed over entirely. probably some circumstance that breaks
+
     using CSV
     #generate file paths
     filepath_csv = joinpath(TASOPT.__TASOPTroot__, "../test/iotest_def.csv")
@@ -74,37 +87,37 @@
     output_csv(ac_def, filepath_csv, indices = output_indices_wGeom)
     
     #test that it creates the files as expected
-    @test isfile(filepath_csv)
-    @test isfile(filepath_csv2)
+    @test_broken isfile(filepath_csv)
+    @test_broken isfile(filepath_csv2)
     
     #pull the files
     csv1 = CSV.File(filepath_csv)
     csv2 = CSV.File(filepath_csv2)
 
     #check row and column counts
-    @test size(csv1,1) == 4 #4 rows w default indices
-    @test size(csv2,1) == 1 #1 row with addl indices
+    @test_broken size(csv1,1) == 4 #4 rows w default indices
+    @test_broken size(csv2,1) == 1 #1 row with addl indices
 
-    @test length(csv1[1]) == 72 # = indices in default_output_indices
-    @test length(csv2[1]) == 97 # = indices in output_indices_wGeom
+    @test_broken length(csv1[1]) == 72 # = indices in default_output_indices
+    @test_broken length(csv2[1]) == 97 # = indices in output_indices_wGeom
 
     #test the nested vector Structures
     #a: row 1 in both csvs matches the design cruise point/mission 
-    @test parse(Float64, string(csv1[1].iaalt)) == ac_def.para[iaalt,ipcruise1,1]
-    @test parse(Float64, string(csv2[1].iaalt)) == ac_def.para[iaalt,ipcruise1,1]
+    @test_broken parse(Float64, string(csv1[1].iaalt)) == ac_def.para[iaalt,ipcruise1,1]
+    @test_broken parse(Float64, string(csv2[1].iaalt)) == ac_def.para[iaalt,ipcruise1,1]
     #b: row 2 has the correct structure (m flight points, n missions)
     #note - for simplicity of imports, evaluate structure by counting brackets
     # since much of the data is parsed as Strings when using CSV.File
     #if more than one mission, one more bracket added
     entry2 = csv1[2].iaalt
     bracket_for_missions = Int( (size(ac_def.parm,2) > 1) ) 
-    @test count(ichar->(ichar=='['), entry2) == iptotal + bracket_for_missions
+    @test_broken count(ichar->(ichar=='['), entry2) == iptotal + bracket_for_missions
     #c: row 3 has the same structure as b despite 1 mission bc forceMatrices
     entry3 = csv1[3].iaalt
-    @test count(ichar->(ichar=='['), entry3) == iptotal + bracket_for_missions
+    @test_broken count(ichar->(ichar=='['), entry3) == iptotal + bracket_for_missions
     #d: row 4 only has flight points, thus 1 bracket
     entry4 = csv1[4].iaalt
-    @test count(ichar->(ichar=='['), entry4) == 1
+    @test_broken count(ichar->(ichar=='['), entry4) == 1
 
     #cleanup
     rm(filepath_csv)
