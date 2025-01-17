186bafdd83e6f2df9572e86e46f7c699dea7065f
Nicolas Gomez <ngomezve@mit.edu>
2025-01-17 10:21:28 -0500

    changed mission inputs in offdesign+added HXs

--- src/engine/engine.jl
+++ src/engine/engine.jl
@@ -14,7 +14,7 @@ export tfwrap!, tfcalc!, mcool, Tmcalc, gas_tset, gaschem
 export tfweightwrap!, tfweight, ddct, ddat, gct, gat, tfsize!, Ncmap, ecmap, Ncmap1, ecmap1, etmap, Pimap, tfoper!
 
 export gassum, gassumd, gas_prat, gas_delh, gas_delhd, gas_burn, gas_burnd, gas_mach, gas_machd, gas_mass, gasfuel, fuelLHV, gasPr
-export hxdesign!, radiator_design!, hxweight, resetHXs
+export hxdesign!, radiator_design!, hxweight, resetHXs, HXOffDesign!
 
 import ..TASOPT: __TASOPTindices__, __TASOPTroot__, StructuralAlloy
 
