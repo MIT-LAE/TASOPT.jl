336ea9e6342ccdcd6accb14266b3ac2fe9741022
prateekr9SF <119811887+prateekr9SF@users.noreply.github.com>
2025-02-11 02:38:55 -0500

    add linear elastic K

--- sandbox/Composites/stiffness.py
+++ sandbox/Composites/stiffness.py
@@ -25,3 +25,56 @@ def compute_ABD_matrices(E1, E2, G12, nu12, ply_angles, t):
     return ABD
 
 
+def compute_stiffness_matrix(E, nu, thickness, element_nodes, element_connectivity):
+    """
+    Computes the global stiffness matrix for a 2D linear elastic homogeneous material.
+    
+    Parameters:
+    E : float
+        Young's modulus of the material
+    nu : float
+        Poisson's ratio of the material
+    thickness : float
+        Thickness of the 2D plane stress/strain model
+    element_nodes : ndarray
+        Nodal coordinates of the elements
+    element_connectivity : ndarray
+        Element connectivity matrix
+    
+    Returns:
+    K_global : sparse matrix
+        The assembled global stiffness matrix
+    """
+    num_nodes = element_nodes.shape[0]
+    K_global = sp.lil_matrix((2 * num_nodes, 2 * num_nodes))
+    
+    C = (E / (1 - nu**2)) * np.array([
+        [1, nu, 0],
+        [nu, 1, 0],
+        [0, 0, (1 - nu) / 2]
+    ])
+    
+    for elem in element_connectivity:
+        node_indices = elem
+        coords = element_nodes[node_indices]
+        
+        # Compute element stiffness matrix (assuming linear triangular elements)
+        x = coords[:, 0]
+        y = coords[:, 1]
+        A = 0.5 * abs(x[0] * (y[1] - y[2]) + x[1] * (y[2] - y[0]) + x[2] * (y[0] - y[1]))
+        
+        B = np.array([
+            [y[1] - y[2], 0, y[2] - y[0], 0, y[0] - y[1], 0],
+            [0, x[2] - x[1], 0, x[0] - x[2], 0, x[1] - x[0]],
+            [x[2] - x[1], y[1] - y[2], x[0] - x[2], y[2] - y[0], x[1] - x[0], y[0] - y[1]]
+        ]) / (2 * A)
+        
+        k_elem = thickness * A * (B.T @ C @ B)
+        
+        # Assemble into global stiffness matrix
+        dof_indices = np.ravel([[2 * i, 2 * i + 1] for i in node_indices])
+        for i in range(6):
+            for j in range(6):
+                K_global[dof_indices[i], dof_indices[j]] += k_elem[i, j]
+    
+    return K_global.tocsr()
