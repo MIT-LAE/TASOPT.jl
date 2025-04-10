698eb02ba01f1ee8f8e04031dd1c87cbfdbb55fa
Jonas J. Gonzalez <argonaut@mit.edu>
2025-04-10 00:46:04 -0400

    deleted `src/propsys/` and contents as part of NPSS excision. Doesn't seem to be used at all

--- src/TASOPT.jl
+++ src/TASOPT.jl
@@ -53,7 +53,6 @@ include(joinpath(__TASOPTroot__,"mission/takeoff.jl"))
 include(joinpath(__TASOPTroot__,"aero/aero.jl"))
 export plot_airf
 include(joinpath(__TASOPTroot__,"structures/structures.jl"))
-include(joinpath(__TASOPTroot__,"propsys/propsys.jl"))
 include(joinpath(__TASOPTroot__,"balance/balance.jl"))
 include(joinpath(__TASOPTroot__,"engine/engine.jl"))
 
@@ -65,9 +64,7 @@ export fuselage_tank
 using .atmosphere
 using .aerodynamics
 using .structures
-using .propsys
 using .engine
-
 
 # Load primary aircraft structure 
 include(joinpath(__TASOPTroot__,"data_structs/landing_gear.jl"))
