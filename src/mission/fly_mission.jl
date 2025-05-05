dd18ad2f8f9b178cf334fe686d7d84bbf37ba13f
Nicolas Gomez <ngomezve@mit.edu>
2025-05-05 13:44:56 -0400

    calculating CL from alt in off-design

--- src/mission/fly_mission.jl
+++ src/mission/fly_mission.jl
@@ -236,20 +236,20 @@ function fly_mission!(ac, imission = 1; itermax = 35, initializes_engine = true)
           rlx = 0.5
     end
 
-    #Calculate start-of-cruise altitude from desired lift coefficient
+    # Calculate start-of-cruise altitude from desired cruise altitude
     # Use cabin volume to get buoyancy weight
+    ρ0 = pare[ierho0, ipcruise1]
     ρcab = max(parg[igpcabin], pare[iep0, ipcruise1]) / (RSL * TSL)
-    WbuoyCR = (ρcab - pare[ierho0, ipcruise1]) * gee * parg[igcabVol]
+    WbuoyCR = (ρcab - ρ0) * gee * parg[igcabVol]
 
     ip = ipcruise1
     We = WMTO * para[iafracW, ip]
-    CL = para[iaCL, ip]
     u0 = pare[ieu0, ip]
     BW = We + WbuoyCR # Weight including buoyancy
     S = wing.layout.S
 
-    ρ0 = BW / (0.5*u0^2*S*CL) #Find density from L=W
-    para[iaalt, ip] = find_altitude_from_density(ρ0, ΔTatmos) * 1e3 #Store altitude
+    CL = BW / (0.5*u0^2*S*ρ0) #Find density from L=W
+    para[iaCL, ip] = CL
 
     set_ambient_conditions!(ac, ipcruise1, im = imission)
 
