2d6fc3eba7df4eacc7d8ec3df8aa80dd9b297250
Jonas J. Gonzalez <argonaut@mit.edu>
2026-05-14 17:45:36 -0400

    Off-design bug fixed through rebase and increase itermax 🤷. off-design regression tests updated

--- test/regression_test_size_aircraft.jl
+++ test/regression_test_size_aircraft.jl
@@ -36,14 +36,10 @@ end
 #Simple function to call fly_mission!() and test on- and off-design performance
 function test_ac_off_design(ac, PFEI, Wfuel, WTO)
     @testset "Off-design" begin
-        try
-            TASOPT.fly_mission!(ac, 2; printTO=false)
-            @test_broken ac.parm[imPFEI, 2] ≈ PFEI rtol=rtol_sizing
-            @test_broken ac.parm[imWfuel, 2] ≈ Wfuel rtol=rtol_sizing
-            @test_broken ac.parm[imWTO, 2] ≈ WTO rtol=rtol_sizing
-        catch #indicates broken test if fly_mission!() fails
-            @test_broken false
-        end
+        TASOPT.fly_mission!(ac, 2; printTO=false, itermax=100)
+        @test ac.parm[imPFEI, 2] ≈ PFEI rtol=rtol_sizing
+        @test ac.parm[imWfuel, 2] ≈ Wfuel rtol=rtol_sizing
+        @test ac.parm[imWTO, 2] ≈ WTO rtol=rtol_sizing
     end
 end
 
@@ -95,7 +91,7 @@ end
         end
     end
 
-    test_ac_off_design(ac, 1.0869638391729122, 153128.29535348987,  769359.1150444464)
+    test_ac_off_design(ac, 1.0865038069306052, 153063.48735295353,  769254.9401755023)
     
     @test ac.parm[imPFEI] ≈ 0.9456457746362635  rtol=rtol_sizing
 end
